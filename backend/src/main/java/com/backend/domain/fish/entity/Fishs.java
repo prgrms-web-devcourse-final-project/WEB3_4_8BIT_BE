2220f8804248afece065668be4557fb967e60874
Kim Dong O <kdo_1999@naver.com>
2025-03-25 14:21:21 +0900

    refactor: 엔티티 명 수정

--- backend/src/main/java/com/backend/domain/fish/entity/Fishs.java
+++ backend/src/main/java/com/backend/domain/fish/entity/Fishs.java
@@ -1,8 +1,5 @@
 package com.backend.domain.fish.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.backend.domain.fish.domain.SpawnLocation;
 import com.backend.global.baseentity.BaseEntity;
 
@@ -13,7 +10,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,7 +23,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString
 @EqualsAndHashCode(callSuper = false)
-public class Fish extends BaseEntity {
+public class Fishs extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,8 +38,10 @@ public class Fish extends BaseEntity {
 	@Column(nullable = false)
 	private String icon;
 
+	//TODO 추후 방법 고민 후 다시 추가 예정
 	/*@Column(nullable = false)
 	private List<Long> spawnSeason = new ArrayList<>();*/
+
 
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, length = 10)
