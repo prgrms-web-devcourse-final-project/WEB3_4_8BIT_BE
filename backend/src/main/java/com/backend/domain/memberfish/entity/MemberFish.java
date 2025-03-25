8e0faedfb3fddfd18d3fbb717c06a8138838892c
Kim Dong O <kdo_1999@naver.com>
2025-03-25 12:25:05 +0900

    feat: MemberFish 추가

---
+++ backend/src/main/java/com/backend/domain/memberfish/entity/MemberFish.java
@@ -0,0 +1,37 @@
+package com.backend.domain.memberfish.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.backend.global.baseentity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SuperBuilder
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class MemberFish extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberFishId;
+
+	@Column(nullable = false)
+	@ColumnDefault("0")
+	private Integer count;
+
+	@Column(nullable = false)
+	private Integer length;
+}
