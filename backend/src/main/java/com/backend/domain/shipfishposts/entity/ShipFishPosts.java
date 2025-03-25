7048fdeabe918657fea83796f790dfeebb61de2e
Kim Dong O <kdo_1999@naver.com>
2025-03-25 16:06:04 +0900

    feat: ShipFishPosts 추가

---
+++ backend/src/main/java/com/backend/domain/shipfishposts/entity/ShipFishPosts.java
@@ -0,0 +1,57 @@
+package com.backend.domain.shipfishposts.entity;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import com.backend.global.baseentity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SuperBuilder
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class ShipFishPosts extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long shipFishPostId;
+
+	@Column(nullable = false, length = 50)
+	private String subject;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+
+	//TODO 추후 컬렉션으로 여러 이미지 받게끔 바꿔야함
+	private String images;
+
+	@Column(nullable = false)
+	private BigDecimal price;
+
+	@Column(nullable = false)
+	private ZonedDateTime startDate;
+
+	@Column(nullable = false)
+	private ZonedDateTime endDate;
+
+	//TODO 추후 분 정보 입력
+
+	//TODO 추후 컬렉션으로 수정 예정
+	@Column(nullable = false)
+	private Long fishId;
+
+	private Double reviewEverRate;
+}
