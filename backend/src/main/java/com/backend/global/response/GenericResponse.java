705e20fc85ea46eb163773b8ae3fc543cc90a997
sungkihun <vdvhk12@gmail.com>
2025-03-26 10:26:53 +0900

    chore: GenericResponse 수정

--- backend/src/main/java/com/backend/global/response/GenericResponse.java
+++ backend/src/main/java/com/backend/global/response/GenericResponse.java
@@ -1,10 +1,15 @@
 package com.backend.global.response;
 
 import java.time.ZonedDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 /**
  * GenericResponse
@@ -13,86 +18,20 @@ import org.springframework.http.HttpStatus;
  * @author Kim Dong O
  */
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
 public class GenericResponse<T> {
 
 	private final ZonedDateTime timestamp;
 	private final boolean isSuccess;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private final int code;
 	private final T data;
 	private final String message;
 
-	@Builder(access = AccessLevel.PRIVATE)
-	private GenericResponse(boolean isSuccess, int code, T data, String message) {
-		this.timestamp = ZonedDateTime.now();
-		this.isSuccess = isSuccess;
-		this.code = code;
-		this.data = data;
-		this.message = message;
-	}
-
-	/**
-	 * 요청이 성공하고 code, data, message 있을 때
-	 *
-	 * @param code    응답 코드 값
-	 * @param data    반환 데이터
-	 * @param message 반환 메세지
-	 * @return {@link GenericResponse<T>}
-	 */
-	public static <T> GenericResponse<T> ok(int code, T data, String message) {
-		return GenericResponse.<T>builder()
-			.isSuccess(true)
-			.code(code)
-			.data(data)
-			.message(message)
-			.build();
-	}
-
-	/**
-	 * 요청이 성공하고 code, data 있을 때
-	 *
-	 * @param code    응답 코드 값
-	 * @param data    반환 데이터
-	 * @return {@link GenericResponse<T>}
-	 */
-	public static <T> GenericResponse<T> ok(int code, T data) {
-		return GenericResponse.<T>builder()
-			.isSuccess(true)
-			.code(code)
-			.data(data)
-			.build();
-	}
-
-	/**
-	 * 요청이 성공하고 code, data, message 있을 때
-	 *
-	 * @param code    응답 코드 값
-	 * @param message 반환 메세지
-	 * @return {@link GenericResponse<T>}
-	 */
-	public static <T> GenericResponse<T> ok(int code, String message) {
-		return GenericResponse.<T>builder()
-			.isSuccess(true)
-			.code(code)
-			.message(message)
-			.build();
-	}
-
-	/**
-	 * 요청이 성공하고 code, data, message 있을 때
-	 *
-	 * @param code    응답 코드 값
-	 * @return {@link GenericResponse<T>}
-	 */
-	public static <T> GenericResponse<T> ok(int code) {
-		return GenericResponse.<T>builder()
-			.isSuccess(true)
-			.code(code)
-			.build();
-	}
 
 	/**
 	 * 요청이 성공하고 data, message 있을 때
-	 * <p>code 기본 값 : 200</p>
 	 *
 	 * @param data    반환 데이터
 	 * @param message 반환 메세지
@@ -101,30 +40,13 @@ public class GenericResponse<T> {
 	public static <T> GenericResponse<T> ok(T data, String message) {
 		return GenericResponse.<T>builder()
 			.isSuccess(true)
-			.code(HttpStatus.OK.value())
-			.message(message)
 			.data(data)
-			.build();
-	}
-
-	/**
-	 * 요청이 성공하고 message 있을 때
-	 * <p>code 기본 값 : 200</p>
-	 *
-	 * @param message 반환 메세지
-	 * @return {@link GenericResponse<T>}
-	 */
-	public static <T> GenericResponse<T> ok(String message) {
-		return GenericResponse.<T>builder()
-			.isSuccess(true)
-			.code(HttpStatus.OK.value())
 			.message(message)
 			.build();
 	}
 
 	/**
 	 * 요청이 성공하고 data 있을 때
-	 * <p>code 기본 값 : 200</p>
 	 *
 	 * @param data    반환 데이터
 	 * @return {@link GenericResponse<T>}
@@ -132,8 +54,20 @@ public class GenericResponse<T> {
 	public static <T> GenericResponse<T> ok(T data) {
 		return GenericResponse.<T>builder()
 			.isSuccess(true)
-			.code(HttpStatus.OK.value())
 			.data(data)
+			.build();
+	}
+
+	/**
+	 * 요청이 성공하고 data, message 있을 때
+	 *
+	 * @param message 반환 메세지
+	 * @return {@link GenericResponse<T>}
+	 */
+	public static <T> GenericResponse<T> ok(String message) {
+		return GenericResponse.<T>builder()
+			.isSuccess(true)
+			.message(message)
 			.build();
 	}
 
@@ -146,7 +80,6 @@ public class GenericResponse<T> {
 	public static <T> GenericResponse<T> ok() {
 		return GenericResponse.<T>builder()
 			.isSuccess(true)
-			.code(HttpStatus.OK.value())
 			.build();
 	}
 
