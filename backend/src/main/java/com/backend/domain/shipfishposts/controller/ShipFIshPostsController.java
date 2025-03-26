ad4b969d7f39bafb38a8ce6badb0c459e9219ef1
swjoon <jetkid1446@gmail.com>
2025-03-26 22:12:32 +0900

    Feat: 선상낚시 게시글 controller 생성 api 추가 (MIKKI-68)

--- backend/src/main/java/com/backend/domain/shipfishposts/controller/ShipFIshPostsController.java
+++ backend/src/main/java/com/backend/domain/shipfishposts/controller/ShipFIshPostsController.java
@@ -1,14 +1,42 @@
 package com.backend.domain.shipfishposts.controller;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.domain.shipfishposts.constant.ShipFishPostsMessageConstant;
+import com.backend.domain.shipfishposts.dto.request.ShipFishPostsRequest;
 import com.backend.domain.shipfishposts.service.ShipFishPostsService;
+import com.backend.global.exception.GlobalErrorCode;
+import com.backend.global.exception.GlobalException;
+import com.backend.global.response.GenericResponse;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/ship-post")
 @RequiredArgsConstructor
-public class ShipFIshPostsController {
+public class ShipFishPostsController {
 
-    private final ShipFishPostsService shipFishPostsService;
+	private final ShipFishPostsService shipFishPostsService;
+
+	@PostMapping
+	public GenericResponse<?> createShipFishPost(
+		@RequestBody @Valid ShipFishPostsRequest.Create requestDto,
+		BindingResult bindingResult) {
+
+		// Todo : UserDetails
+
+		if (bindingResult.hasErrors()) {
+			throw new GlobalException(GlobalErrorCode.NOT_VALID);
+		}
+
+		Long shipFishPostsId = shipFishPostsService.save(requestDto);
+
+		return GenericResponse.ok(ShipFishPostsMessageConstant.CREATE_POSTS_SUCCESS);
+	}
 
 }
