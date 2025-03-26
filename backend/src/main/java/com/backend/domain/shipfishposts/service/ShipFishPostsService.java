c053f7c3ccdc03f32008ec36be5bca7614917285
swjoon <jetkid1446@gmail.com>
2025-03-26 22:10:34 +0900

    Feat: 선상낚시 게시글 service save 메소드 구현 (MIKKI-68)

--- backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsService.java
+++ backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsService.java
@@ -1,7 +1,17 @@
 package com.backend.domain.shipfishposts.service;
 
+import com.backend.domain.shipfishposts.dto.request.ShipFishPostsRequest;
+
 public interface ShipFishPostsService {
 
-
+	/**
+	 * 게시물 저장 메소드
+	 *
+	 * @param requestDto {@link ShipFishPostsRequest.Create}
+	 * @return {@link Long ShipFishPostId} Long: 게시글 id
+	 * @implSpec 게시물 정보를 파라미터로 받고 저장한다.
+	 * @author swjoon
+	 */
+	Long save(ShipFishPostsRequest.Create requestDto);
 
 }
