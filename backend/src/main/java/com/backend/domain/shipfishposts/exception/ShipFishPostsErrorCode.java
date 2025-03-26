c80f22ef8d361ca245c849c370f3e3f84ccb92c1
swjoon <jetkid1446@gmail.com>
2025-03-26 22:23:21 +0900

    Fix: 일부 메세지 수정 (MIKKI-68)

--- backend/src/main/java/com/backend/domain/shipfishposts/exception/ShipFishPostsErrorCode.java
+++ backend/src/main/java/com/backend/domain/shipfishposts/exception/ShipFishPostsErrorCode.java
@@ -1,17 +1,18 @@
 package com.backend.domain.shipfishposts.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ShipFishPostsErrorCode {
 
-    POSTS_NOT_FOUND(HttpStatus.NOT_FOUND,7001, "파일 정보가 존재하지 않음");
+	POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, 7001, "게시글 정보가 존재하지 않음");
 
-    private final HttpStatus httpStatus;
-    private final int code;
-    private final String message;
+	private final HttpStatus httpStatus;
+	private final int code;
+	private final String message;
 
 }
