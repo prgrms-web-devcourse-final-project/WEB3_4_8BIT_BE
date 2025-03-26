d3e94e248e652f1cb433458b45028203e4adad75
swjoon <jetkid1446@gmail.com>
2025-03-26 22:10:34 +0900

    Feat: 선상낚시 게시글 service save 메소드 구현 (MIKKI-68)

--- backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsServiceImpl.java
+++ backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsServiceImpl.java
@@ -1,14 +1,28 @@
 package com.backend.domain.shipfishposts.service;
 
-import com.backend.domain.shipfishposts.repository.ShipFishPostsRepository;
-import lombok.RequiredArgsConstructor;
+import java.time.Duration;
+
 import org.springframework.stereotype.Service;
+
+import com.backend.domain.shipfishposts.dto.request.ShipFishPostsRequest;
+import com.backend.domain.shipfishposts.entity.ShipFishPosts;
+import com.backend.domain.shipfishposts.repository.ShipFishPostsRepositoryImpl;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ShipFishPostsServiceImpl implements ShipFishPostsService{
+public class ShipFishPostsServiceImpl implements ShipFishPostsService {
 
-    private final ShipFishPostsRepository shipFishPostsRepository;
+	private final ShipFishPostsRepositoryImpl shipFishPostsRepositoryImpl;
 
+	@Override
+	public Long save(ShipFishPostsRequest.Create requestDto) {
+		long durationMinutes = Duration.between(requestDto.startDate(), requestDto.endDate()).toMinutes();
+
+		ShipFishPosts entity = ShipFishPosts.from(requestDto, durationMinutes);
+
+		return shipFishPostsRepositoryImpl.save(entity).getShipFishPostId();
+	}
 }
 
