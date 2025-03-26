4ffecb223f739992ee66b6a62e534228e876c146
swjoon <jetkid1446@gmail.com>
2025-03-27 02:18:40 +0900

    Feat: 선상낚시 게시글 service 생성 & save 메소드 추가 (MIKKI-28)

---
+++ backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.backend.domain.shipfishposts.service;
+
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.backend.domain.shipfishposts.dto.request.ShipFishPostsRequest;
+import com.backend.domain.shipfishposts.entity.ShipFishPosts;
+import com.backend.domain.shipfishposts.repository.ShipFishPostsRepositoryImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ShipFishPostsServiceImpl implements ShipFishPostsService {
+
+	private final ShipFishPostsRepositoryImpl shipFishPostsRepositoryImpl;
+
+	@Override
+	@Transactional
+	public Long save(ShipFishPostsRequest.Create requestDto) {
+
+		long durationMinutes = Duration.between(requestDto.startDate(), requestDto.endDate()).toMinutes();
+
+		ShipFishPosts entity = ShipFishPosts.from(requestDto, durationMinutes);
+
+		return shipFishPostsRepositoryImpl.save(entity).getShipFishPostId();
+	}
+}
