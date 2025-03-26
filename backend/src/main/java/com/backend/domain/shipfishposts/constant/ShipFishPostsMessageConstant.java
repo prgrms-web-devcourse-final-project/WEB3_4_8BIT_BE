a205dbdde7af15a1e161ad30c5e166b8f9dd893e
swjoon <jetkid1446@gmail.com>
2025-03-26 22:06:37 +0900

    Feat: 선상낚시 Controller 응답 메세지 상수 (MIKKI-68)

---
+++ backend/src/main/java/com/backend/domain/shipfishposts/constant/ShipFishPostsMessageConstant.java
@@ -0,0 +1,5 @@
+package com.backend.domain.shipfishposts.constant;
+
+public abstract class ShipFishPostsMessageConstant {
+	public static final String CREATE_POSTS_SUCCESS = "게시글이 성공적으로 생성되었습니다.";
+}
