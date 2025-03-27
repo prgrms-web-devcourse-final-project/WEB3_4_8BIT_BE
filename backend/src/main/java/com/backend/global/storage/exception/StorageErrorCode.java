09f50fe968f709a57b378574d1f8e4a95d69498e
vdvhk12 <vdvhk12@gmail.com>
2025-03-27 15:54:34 +0900

    refactor: 파일크기 에러코드 제거 (MIKKI-61)
    - 컨트롤러에서 Validation으로 파일 크기 검증

--- backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
+++ backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
@@ -15,9 +15,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum StorageErrorCode {
 
-	//스토리지 에러 코드 500
-	FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, 4001, "파일 크기가 허용된 최대 크기(10MB)를 초과했습니다."),
-	UNSUPPORTED_FILE_TYPE(HttpStatus.BAD_REQUEST, 4002, "지원하지 않는 파일 형식입니다. (허용된 형식: PNG, JPEG, JPG)");
+	//스토리지 에러 코드
+	UNSUPPORTED_FILE_TYPE(HttpStatus.BAD_REQUEST, 4001, "지원하지 않는 파일 형식입니다. (허용된 형식: PNG, JPEG, JPG)");
 
 	private final HttpStatus httpStatus;
 	private final int code;
