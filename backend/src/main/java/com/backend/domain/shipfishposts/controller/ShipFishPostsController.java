1d8f7d3f9fdd349f917eee1e16332dd649bb2b52
swjoon <jetkid1446@gmail.com>
2025-03-27 14:24:10 +0900

    Fix: ShipFishPost Controller swagger 적용 (MIKKI-28)

--- backend/src/main/java/com/backend/domain/shipfishposts/controller/ShipFishPostsController.java
+++ backend/src/main/java/com/backend/domain/shipfishposts/controller/ShipFishPostsController.java
@@ -1,18 +1,19 @@
 package com.backend.domain.shipfishposts.controller;
 
-import org.springframework.validation.BindingResult;
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.backend.domain.shipfishposts.constant.ShipFishPostsMessageConstant;
 import com.backend.domain.shipfishposts.dto.request.ShipFishPostsRequest;
 import com.backend.domain.shipfishposts.service.ShipFishPostsService;
-import com.backend.global.exception.GlobalErrorCode;
-import com.backend.global.exception.GlobalException;
 import com.backend.global.response.GenericResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -23,20 +24,20 @@ public class ShipFishPostsController {
 
 	private final ShipFishPostsService shipFishPostsService;
 
+	@Tag(name = "선상낚시 게시글 > 게시글 생성 API 명세")
+	@Operation(
+		summary = "선상 낚시 게시글 생성",
+		description = "유저가 새로운 선상 낚시 게시글을 생성할 때 사용하는 API"
+	)
 	@PostMapping
-	public GenericResponse<?> createShipFishPost(
-		@RequestBody @Valid ShipFishPostsRequest.Create requestDto,
-		BindingResult bindingResult) {
+	public ResponseEntity<GenericResponse<Void>> createShipFishPost(
+		@RequestBody @Valid ShipFishPostsRequest.Create requestDto) {
 
 		// Todo : UserDetails
 
-		if (bindingResult.hasErrors()) {
-			throw new GlobalException(GlobalErrorCode.NOT_VALID);
-		}
-
 		Long shipFishPostsId = shipFishPostsService.save(requestDto);
 
-		return GenericResponse.ok(ShipFishPostsMessageConstant.CREATE_POSTS_SUCCESS);
+		return ResponseEntity.created(URI.create(shipFishPostsId.toString())).build();
 	}
 
 }
