3b8a0e0f701426b2d8b83f6182aaa43ee6bda0ae
vdvhk12 <vdvhk12@gmail.com>
2025-03-27 11:11:39 +0900

    chore: 에러 코드 주석 수정 (MIKKI-61)

--- backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
+++ backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
@@ -5,11 +5,17 @@ import org.springframework.http.HttpStatus;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * StorageErrorCode
+ * <p>예외 발생시 사용할 ErrorCode 입니다. <br>
+ *
+ * @author vdvhk12
+ */
 @Getter
 @RequiredArgsConstructor
 public enum StorageErrorCode {
 
-	//공통 서버 에러 코드 500
+	//스토리지 에러 코드 500
 	FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, 4001, "파일 크기가 허용된 최대 크기(10MB)를 초과했습니다."),
 	UNSUPPORTED_FILE_TYPE(HttpStatus.BAD_REQUEST, 4002, "지원하지 않는 파일 형식입니다. (허용된 형식: PNG, JPEG, JPG)");
 
--- backend/src/main/java/com/backend/global/storage/exception/StorageException.java
+++ backend/src/main/java/com/backend/global/storage/exception/StorageException.java
@@ -4,15 +4,33 @@ import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
 
+/**
+ * StorageException
+ * <p>스토리지 예외 클래스 입니다. <br><br>
+ * 사용 예시: </p>
+ * {@code
+ * throw new StorageException(StorageErrorCode.FILE_SIZE_EXCEEDED);
+ * }
+ * @author vdvhk12
+ */
 @Getter
 public class StorageException extends RuntimeException {
+
 	private final StorageErrorCode storageErrorCode;
 
+	/**
+	 * GlobalException 생성자 입니다.
+	 * @param storageErrorCode StorageErrorCode 값
+	 */
 	public StorageException(StorageErrorCode storageErrorCode) {
 		super(storageErrorCode.getMessage());
 		this.storageErrorCode = storageErrorCode;
 	}
 
+	/**
+	 * 응답 HttpStatus를 반환하는 메서드 입니다.
+	 * @return {@link HttpStatus}
+	 */
 	public HttpStatus getStatus() {
 		return storageErrorCode.getHttpStatus();
 	}
