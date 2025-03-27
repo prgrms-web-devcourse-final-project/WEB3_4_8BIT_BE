cc22eaaddcc680cec81fb19513bb96379c23e4c6
vdvhk12 <vdvhk12@gmail.com>
2025-03-27 15:56:39 +0900

    chore: 불필요한 주석 제거 (MIKKI-61)

--- backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
+++ backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum StorageErrorCode {
 
-	//스토리지 에러 코드
 	UNSUPPORTED_FILE_TYPE(HttpStatus.BAD_REQUEST, 4001, "지원하지 않는 파일 형식입니다. (허용된 형식: PNG, JPEG, JPG)");
 
 	private final HttpStatus httpStatus;
