95b8d21acdd9e6943027d5d3ff22584f4c778bd3
Kim Dong O <kdo_1999@naver.com>
2025-03-27 12:25:40 +0900

    test: 물고기 도감 저장 테스트 수정 (MIKKI-69)
    FixtureMonkey 도입으로 인한 수정

--- backend/src/test/java/com/backend/domain/fishencyclopedia/repository/FishEncyclopediasRepositoryTest.java
+++ backend/src/test/java/com/backend/domain/fishencyclopedia/repository/FishEncyclopediasRepositoryTest.java
@@ -12,32 +12,45 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Repository;
 
 import com.backend.domain.fishencyclopedia.entity.FishEncyclopedias;
-import com.backend.domain.fishencyclopedia.util.FishEncyclopediaUtil;
 import com.backend.global.config.JpaAuditingConfig;
 
 import lombok.extern.slf4j.Slf4j;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
 
 @Slf4j
 @Import(JpaAuditingConfig.class)
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 class FishEncyclopediasRepositoryTest {
+
 	private final FishEncyclopediaRepository fishEncyclopediaRepository;
+	private final FixtureMonkey fixtureMonkeyBuilder;
 
 	@Autowired
 	public FishEncyclopediasRepositoryTest(FishEncyclopediaRepository fishEncyclopediaRepository) {
 		this.fishEncyclopediaRepository = fishEncyclopediaRepository;
+
+		fixtureMonkeyBuilder = FixtureMonkey.builder()
+			.objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
+			.defaultNotNull(true)
+			.build();
 	}
 
 	@Test
-	@DisplayName("물고기 도감 저장 성공 테스트")
+	@DisplayName("물고기 도감 저장 [Repository] - Success")
 	void t01() {
+
 		//given
-		FishEncyclopedias givenFishEncyclopedias = FishEncyclopediaUtil.createFishEncyclopediaWithNullableId();
+		FishEncyclopedias givenFishEncyclopedias = fixtureMonkeyBuilder
+			.giveMeBuilder(FishEncyclopedias.class)
+			.set("fishEncyclopediaId", null)
+			.sample();
 
 		//when
 		FishEncyclopedias savedFishEncyclopedias = fishEncyclopediaRepository.save(givenFishEncyclopedias);
 
 		//then
-		assertThat(savedFishEncyclopedias.getFishId()).isNotNull();
+		assertThat(savedFishEncyclopedias).isNotNull();
 	}
 }
