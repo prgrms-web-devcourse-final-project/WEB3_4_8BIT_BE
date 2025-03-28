d865530a5ba486ea18102e68890a92f957be2205
vdvhk12 <vdvhk12@gmail.com>
2025-03-28 17:43:47 +0900

    refactor: 리포지토리 인터페이스 분리 (MIKKI-73)

---
+++ backend/src/main/java/com/backend/domain/review/repository/ReviewJpaRepository.java
@@ -0,0 +1,9 @@
+package com.backend.domain.review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.domain.review.entity.Review;
+
+public interface ReviewJpaRepository extends JpaRepository<Review, Long> {
+
+}
