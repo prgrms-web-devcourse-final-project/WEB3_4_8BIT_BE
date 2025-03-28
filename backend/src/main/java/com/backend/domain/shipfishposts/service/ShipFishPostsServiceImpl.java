a690b4af6096db000028fdd2f39d455c13447c7d
swjoon <jetkid1446@gmail.com>
2025-03-28 12:22:33 +0900

    Fix: ShipFishPost Service 일부 수정 (MIKKI-28) - duration 메서드 분리

--- backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsServiceImpl.java
+++ backend/src/main/java/com/backend/domain/shipfishposts/service/ShipFishPostsServiceImpl.java
@@ -1,17 +1,20 @@
 package com.backend.domain.shipfishposts.service;
 
 import java.time.Duration;
+import java.time.LocalTime;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.backend.domain.shipfishposts.dto.converter.ShipFishPostsConverter;
+import com.backend.domain.shipfishposts.converter.ShipFishPostsConverter;
 import com.backend.domain.shipfishposts.dto.request.ShipFishPostsRequest;
 import com.backend.domain.shipfishposts.entity.ShipFishPosts;
 import com.backend.domain.shipfishposts.repository.ShipFishPostsRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ShipFishPostsServiceImpl implements ShipFishPostsService {
@@ -20,12 +23,38 @@ public class ShipFishPostsServiceImpl implements ShipFishPostsService {
 
 	@Override
 	@Transactional
-	public Long save(ShipFishPostsRequest.Create requestDto) {
+	public Long save(final ShipFishPostsRequest.Create requestDto) {
 
-		long durationMinutes = Duration.between(requestDto.startDate(), requestDto.endDate()).toMinutes();
+		String durationTime = durationToString(requestDto.startTime(), requestDto.endTime());
 
-		ShipFishPosts entity = ShipFishPostsConverter.fromShipFishPostsRequestCreate(requestDto, durationMinutes);
+		ShipFishPosts entity = ShipFishPostsConverter.fromShipFishPostsRequestCreate(requestDto, durationTime);
+
+		// Todo : 예약 불가날짜 관리 로직 필요
+		// requestDto.unavailableDates();
+
+		log.debug("Save ship fish posts: {}", entity);
 
 		return shipFishPostsRepository.save(entity).getShipFishPostId();
+	}
+
+	/**
+	 * 시작시간, 마감시간 사이 기간 String 으로 변환 메서드
+	 *
+	 * @param startTime
+	 * @param endTime
+	 * @return {@link String duration}
+	 */
+	private String durationToString(final LocalTime startTime, final LocalTime endTime) {
+		Duration durationTime = Duration.between(startTime, endTime);
+
+		if (durationTime.isNegative()) {
+			durationTime = durationTime.plusDays(1);
+		}
+
+		long totalMinutes = durationTime.toMinutes();
+		long hours = totalMinutes / 60;
+		long minutes = totalMinutes % 60;
+
+		return String.format("%02d:%02d", hours, minutes);
 	}
 }
