d4d2819060cbe4bd9108357d8e94dfe04c7c150a
Kim Dong O <kdo_1999@naver.com>
2025-03-30 20:39:27 +0900

    test: 낚시 포인트 저장, 중복 체크 메소드 테스트 작성 (MIKKI-69)

---
+++ backend/src/test/java/com/backend/domain/fishpoint/repository/FishPointRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.backend.domain.fishpoint.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.backend.domain.fishpoint.entity.FishPoint;
+import com.backend.global.Util.BaseTest;
+import com.backend.global.config.JpaAuditingConfig;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.navercorp.fixturemonkey.ArbitraryBuilder;
+
+@Import({FishPointRepositoryImpl.class, JpaAuditingConfig.class})
+@DataJpaTest
+@Slf4j
+class FishPointRepositoryTest extends BaseTest {
+
+	@Autowired
+	private FishPointRepository fishPointRepository;
+
+	final ArbitraryBuilder<FishPoint> arbitraryBuilder = fixtureMonkeyBuilder.giveMeBuilder(FishPoint.class)
+		.size("fishPointName", 1, 49)
+		.size("fishPointDetailName", 1, 49);
+
+	@Test
+	@DisplayName("낚시 포인트 저장 [Repository] - Success")
+	void t01() {
+		// Given
+		FishPoint givenFishPoint = arbitraryBuilder.set("fishPointId", null).sample();
+
+		log.info(givenFishPoint.toString());
+
+		// When
+		FishPoint savedFishPoint = fishPointRepository.save(givenFishPoint);
+
+		// Then
+		assertThat(savedFishPoint.getFishPointId()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("낚시 포인트 존재 여부 조회 [Repository] - Success")
+	void t02() {
+		// Given
+		FishPoint givenFishPoint = arbitraryBuilder.set("fishPointId", null).sample();
+		FishPoint savedFishPoint = fishPointRepository.save(givenFishPoint);
+
+		// When
+		boolean existsFishPoint = fishPointRepository.existsById(savedFishPoint.getFishPointId());
+
+		// Then
+		assertThat(existsFishPoint).isTrue();
+	}
+}
