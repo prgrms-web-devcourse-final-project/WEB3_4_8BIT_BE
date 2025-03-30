435c15b9bcf87d316bfe43c47350bbbdbdac9126
swjoon <jetkid1446@gmail.com>
2025-03-30 13:03:11 +0900

    feat : shipfishingpost 게시글 상세조회 메서드 (MIKKI-77) - controller

--- backend/src/main/java/com/backend/domain/shipfishingpost/controller/ShipFishingPostController.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/controller/ShipFishingPostController.java
@@ -3,16 +3,20 @@ package com.backend.domain.shipfishingpost.controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.domain.shipfishingpost.dto.request.ShipFishingPostRequest;
+import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.backend.domain.shipfishingpost.service.ShipFishingPostService;
 import com.backend.global.response.GenericResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,15 +29,28 @@ public class ShipFishingPostController {
 
 	private final ShipFishingPostService shipFishingPostService;
 
-	@Operation(summary = "선상 낚시 게시글 생성", description = "유저가 새로운 선상 낚시 게시글을 생성할 때 사용하는 API")
 	@PostMapping
+	@Operation(summary = "선상 낚시 게시글 생성", description = "유저가 새로운 선상 낚시 게시글을 생성할 때 사용하는 API")
 	public ResponseEntity<GenericResponse<Void>> createShipFishPost(
-		@RequestBody @Valid ShipFishingPostRequest.Create requestDto) {
+		@RequestBody @Valid final ShipFishingPostRequest.Create requestDto
+	) {
 
 		// Todo : UserDetails
 
 		Long shipFishPostsId = shipFishingPostService.save(requestDto);
 
 		return ResponseEntity.created(URI.create(shipFishPostsId.toString())).body(GenericResponse.of(true));
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary = "선상 낚시 게시글 상세 조회", description = "유저가 선상 낚시 게시글을 상세 조회할 때 사용하는 API")
+	@Parameter(name = "id", description = "조회할 선상 낚시 게시글 ID", example = "1")
+	public ResponseEntity<GenericResponse<ShipFishingPostResponse.Detail>> getShipFishPost(
+		@PathVariable("id") final Long shipFishPostsId
+	) {
+
+		ShipFishingPostResponse.Detail response = shipFishingPostService.getShipFishingPost(shipFishPostsId);
+
+		return ResponseEntity.ok(GenericResponse.of(true, response));
 	}
 }
