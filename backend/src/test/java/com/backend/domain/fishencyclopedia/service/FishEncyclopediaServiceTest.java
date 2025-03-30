c67391d11daac74d8b7b3f5e31a87da8d538a1ae
Kim Dong O <kdo_1999@naver.com>
2025-03-30 20:40:07 +0900

    test: 물고기 도감 실패 테스트 작성 (MIKKI-69)

--- backend/src/test/java/com/backend/domain/fishencyclopedia/service/FishEncyclopediaServiceTest.java
+++ backend/src/test/java/com/backend/domain/fishencyclopedia/service/FishEncyclopediaServiceTest.java
@@ -8,13 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.backend.domain.fishencyclopedia.converter.FishEncyclopediaConverter;
 import com.backend.domain.fishencyclopedia.dto.request.FishEncyclopediaRequest;
 import com.backend.domain.fishencyclopedia.entity.FishEncyclopedia;
+import com.backend.domain.fishencyclopedia.exception.FishEncyclopediaErrorCode;
+import com.backend.domain.fishencyclopedia.exception.FishEncyclopediaException;
 import com.backend.domain.fishencyclopedia.repository.FishEncyclopediaRepository;
+import com.backend.domain.fishpoint.repository.FishPointRepository;
 import com.backend.domain.member.entity.Member;
 import com.backend.global.Util.BaseTest;
 
@@ -24,13 +26,49 @@ class FishEncyclopediaServiceTest extends BaseTest {
 	@Mock
 	private FishEncyclopediaRepository fishEncyclopediaRepository;
 
+	@Mock
+	private FishPointRepository fishPointRepository;
+
 	@InjectMocks
 	private FishEncyclopediaServiceImpl fishEncyclopediasService;
 
 	@Test
 	@DisplayName("물고기 도감 저장 [Service] - Success")
 	void t01() {
-		//given
+		// Given
+		FishEncyclopediaRequest.Create givenCreate = fixtureMonkeyValidation.giveMeOne(
+			FishEncyclopediaRequest.Create.class);
+
+		Member givenMember = fixtureMonkeyBuilder.giveMeOne(Member.class);
+
+		FishEncyclopedia givenFromFishEncyclopedia = FishEncyclopediaConverter.fromFishEncyclopediasRequestCreate(
+			givenCreate,
+			givenMember.getMemberId()
+		);
+
+		FishEncyclopedia givenFishEncyclopedia = fixtureMonkeyBuilder.giveMeBuilder(FishEncyclopedia.class)
+			.set("fishEncyclopediaId", 1L)
+			.set("fishId", givenCreate.fishId())
+			.set("length", givenCreate.length())
+			.set("fishPointId", givenCreate.fishPointId())
+			.set("memberId", givenMember.getMemberId())
+			.sample();
+
+		when(fishEncyclopediaRepository.save(givenFromFishEncyclopedia)).thenReturn(givenFishEncyclopedia);
+		when(fishPointRepository.existsById(givenFishEncyclopedia.getFishPointId())).thenReturn(true);
+
+		// When
+		Long savedId = fishEncyclopediasService.save(givenCreate, givenMember.getMemberId());
+
+		// Then
+		assertThat(savedId).isEqualTo(givenFishEncyclopedia.getFishEncyclopediaId());
+		verify(fishEncyclopediaRepository, times(1)).save(givenFromFishEncyclopedia);
+	}
+
+	@Test
+	@DisplayName("물고기 도감 저장 [Fish Not Exists] [Service] - Fail")
+	void t02() {
+		// Given
 		FishEncyclopediaRequest.Create givenCreate = fixtureMonkeyValidation.giveMeOne(
 			FishEncyclopediaRequest.Create.class);
 
@@ -48,24 +86,12 @@ class FishEncyclopediaServiceTest extends BaseTest {
 			.set("memberId", givenMember.getMemberId())
 			.sample();
 
-		Mockito.when(fishEncyclopediaRepository.save(givenFromFishEncyclopedia)).thenReturn(givenFishEncyclopedia);
+		when(fishPointRepository.existsById(givenFishEncyclopedia.getFishPointId())).thenReturn(false);
 
-		//when
-		Long savedId = fishEncyclopediasService.save(givenCreate, givenMember.getMemberId());
-
-		//then
-		assertThat(savedId).isEqualTo(givenFishEncyclopedia.getFishEncyclopediaId());
-		verify(fishEncyclopediaRepository, times(1)).save(givenFromFishEncyclopedia);
-	}
-
-	@Test
-	@DisplayName("물고기 도감 저장 [Fish Not Exists] [Service] - Fail")
-	void t02() {
-		//given
-
-		//when
-
-		//then
+		// When & Then
+		assertThatThrownBy(() -> fishEncyclopediasService.save(givenCreate, givenMember.getMemberId()))
+			.isExactlyInstanceOf(FishEncyclopediaException.class)
+			.hasMessage(FishEncyclopediaErrorCode.NOT_EXISTS_FISH_POINT.getMessage());
 	}
 
 	@Test
