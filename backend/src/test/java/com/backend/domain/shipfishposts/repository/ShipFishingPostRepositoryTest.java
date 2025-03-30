9d7ff2924198d43deb4cee5f02d465416bdbb22b
swjoon <jetkid1446@gmail.com>
2025-03-30 18:33:24 +0900

    test: ShipFishingPost 새로운 상세 조회 메서드 테스트 코드 작성 (MIKKI-77) - serviceTest - repositoryTest

--- backend/src/test/java/com/backend/domain/shipfishposts/repository/ShipFishingPostRepositoryTest.java
+++ backend/src/test/java/com/backend/domain/shipfishposts/repository/ShipFishingPostRepositoryTest.java
@@ -2,6 +2,8 @@ package com.backend.domain.shipfishposts.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,9 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Repository;
 
+import com.backend.domain.ship.dto.response.ShipResponse;
+import com.backend.domain.ship.entity.Ship;
+import com.backend.domain.ship.repository.ShipRepository;
 import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.backend.domain.shipfishingpost.entity.ShipFishingPost;
 import com.backend.domain.shipfishingpost.repository.ShipFishingPostRepository;
@@ -25,6 +30,9 @@ import com.navercorp.fixturemonkey.ArbitraryBuilder;
 @Import(QuerydslConfig.class)
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 public class ShipFishingPostRepositoryTest extends BaseTest {
+
+	@Autowired
+	private ShipRepository shipRepository;
 
 	@Autowired
 	private ShipFishingPostRepository shipFishingPostRepository;
@@ -50,7 +58,7 @@ public class ShipFishingPostRepositoryTest extends BaseTest {
 	}
 
 	@Test
-	@DisplayName("선상 낚시 게시글 상세 조회 [Repository] - Success")
+	@DisplayName("선상 낚시 게시글 상세 조회 [ShipFishingPostResponse.Detail] [Repository] - Success")
 	void t02() {
 		// Given
 		ShipFishingPost givenShipFishingPost = arbitraryBuilder
@@ -72,5 +80,64 @@ public class ShipFishingPostRepositoryTest extends BaseTest {
 		assertThat(findDetail.durationTime()).isEqualTo(savedShipFishingPost.getDurationTime());
 		assertThat(findDetail.maxGuestCount()).isEqualTo(savedShipFishingPost.getMaxGuestCount());
 		assertThat(findDetail.reviewEverRate()).isEqualTo(savedShipFishingPost.getReviewEverRate());
+	}
+
+	@Test
+	@DisplayName("선상 낚시 게시글 상세 조회 [ShipFishingPostResponse.DetailAll] [Repository] - Success")
+	void t03() {
+		// Given
+		Ship givenShip = fixtureMonkeyBuilder.giveMeBuilder(Ship.class)
+			.set("shipId", null)
+			.set("shipName", "나로호")
+			.set("shipNumber", "12345-6789")
+			.set("departurePort", "선착장")
+			.sample();
+
+		Ship savedShip = shipRepository.save(givenShip);
+
+		ShipFishingPost givenShipFishingPost = arbitraryBuilder
+			.set("shipFishingPostId", null)
+			.set("shipId", savedShip.getShipId())
+			.sample();
+
+		ShipFishingPost savedShipFishingPost = shipFishingPostRepository.save(givenShipFishingPost);
+
+		// When
+		Optional<ShipFishingPostResponse.DetailAll> findOptionalDetailAll = shipFishingPostRepository
+			.findDetailAllById(savedShipFishingPost.getShipFishingPostId());
+
+		// Then
+		assertThat(findOptionalDetailAll).isPresent();
+
+		ShipFishingPostResponse.DetailAll findDetailAll = findOptionalDetailAll.get();
+
+		// 선상 낚시 게시글 정보 검증
+		ShipFishingPostResponse.Detail postDetail = findDetailAll.detailShipFishingPost();
+
+		assertThat(postDetail.shipFishingPostId()).isEqualTo(savedShipFishingPost.getShipFishingPostId());
+		assertThat(postDetail.subject()).isEqualTo(savedShipFishingPost.getSubject());
+		assertThat(postDetail.content()).isEqualTo(savedShipFishingPost.getContent());
+		assertThat(postDetail.imageList().toString()).isEqualTo(savedShipFishingPost.getImageList().toString());
+		assertThat(postDetail.startTime()).isEqualTo(savedShipFishingPost.getStartTime());
+		assertThat(postDetail.durationTime()).isEqualTo(savedShipFishingPost.getDurationTime());
+		assertThat(postDetail.maxGuestCount()).isEqualTo(savedShipFishingPost.getMaxGuestCount());
+		assertThat(postDetail.reviewEverRate()).isEqualTo(savedShipFishingPost.getReviewEverRate());
+
+		// 선박 정보 검증
+		ShipResponse.Detail shipDetail = findDetailAll.detailShip();
+		assertThat(shipDetail.shipId()).isEqualTo(savedShip.getShipId());
+		assertThat(shipDetail.shipName()).isEqualTo(savedShip.getShipName());
+		assertThat(shipDetail.shipNumber()).isEqualTo(savedShip.getShipNumber());
+		assertThat(shipDetail.departurePort()).isEqualTo(savedShip.getDeparturePort());
+		assertThat(shipDetail.publicRestroom()).isEqualTo(savedShip.getPublicRestroom());
+		assertThat(shipDetail.loungeArea()).isEqualTo(savedShip.getLoungeArea());
+		assertThat(shipDetail.kitchenFacility()).isEqualTo(savedShip.getKitchenFacility());
+		assertThat(shipDetail.fishingChair()).isEqualTo(savedShip.getFishingChair());
+		assertThat(shipDetail.passengerInsurance()).isEqualTo(savedShip.getPassengerInsurance());
+		assertThat(shipDetail.fishingGearRental()).isEqualTo(savedShip.getFishingGearRental());
+		assertThat(shipDetail.mealProvided()).isEqualTo(savedShip.getMealProvided());
+		assertThat(shipDetail.parkingAvailable()).isEqualTo(savedShip.getParkingAvailable());
+
+		// Todo : 멤버 정보 검증
 	}
 }
--- backend/src/test/java/com/backend/domain/shipfishposts/service/ShipFishingPostServiceTest.java
+++ backend/src/test/java/com/backend/domain/shipfishposts/service/ShipFishingPostServiceTest.java
@@ -13,6 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import com.backend.domain.ship.entity.Ship;
+import com.backend.domain.ship.exception.ShipErrorCode;
+import com.backend.domain.ship.exception.ShipException;
+import com.backend.domain.ship.repository.ShipRepository;
 import com.backend.domain.shipfishingpost.converter.ShipFishingPostConverter;
 import com.backend.domain.shipfishingpost.dto.request.ShipFishingPostRequest;
 import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
@@ -26,11 +30,14 @@ import com.backend.global.Util.BaseTest;
 @ExtendWith(MockitoExtension.class)
 public class ShipFishingPostServiceTest extends BaseTest {
 
-	@InjectMocks
-	private ShipFishingPostServiceImpl shipFishingPostServiceImpl;
+	@Mock
+	private ShipRepository shipRepository;
 
 	@Mock
 	private ShipFishingPostRepository shipFishingPostRepository;
+
+	@InjectMocks
+	private ShipFishingPostServiceImpl shipFishingPostServiceImpl;
 
 	@Test
 	@DisplayName("선상 낚시 게시글 저장 [Service] - Success")
@@ -39,22 +46,70 @@ public class ShipFishingPostServiceTest extends BaseTest {
 		ShipFishingPostRequest.Create givenRequestDto = fixtureMonkeyValidation.giveMeOne(
 			ShipFishingPostRequest.Create.class);
 
-		ShipFishingPost givenShipFishingPost = ShipFishingPostConverter.fromShipFishPostsRequestCreate(givenRequestDto);
+		Ship givenShip = fixtureMonkeyBuilder.giveMeBuilder(Ship.class)
+			.set("memberId", 1L).sample();
+
+		ShipFishingPost givenShipFishingPost = ShipFishingPostConverter.fromShipFishPostsRequestCreate(givenRequestDto,
+			1L);
 
 		ReflectionTestUtils.setField(givenShipFishingPost, "shipFishingPostId", 1L);
 
 		// When
+		when(shipRepository.findById(any(Long.class))).thenReturn(Optional.ofNullable(givenShip));
 		when(shipFishingPostRepository.save(any(ShipFishingPost.class))).thenReturn(givenShipFishingPost);
 
-		Long savedId = shipFishingPostServiceImpl.save(givenRequestDto);
+		Long savedId = shipFishingPostServiceImpl.saveShipFishingPost(givenRequestDto, 1L);
 
 		// Then
 		assertThat(savedId).isEqualTo(1L);
 	}
 
 	@Test
-	@DisplayName("선상 낚시 게시글 상세 조회 [Service] - Success")
+	@DisplayName("선상 낚시 게시글 저장 [SHIP_NOT_FOUND] [Service] - Fail")
 	void t02() {
+		// Given
+		ShipFishingPostRequest.Create givenRequestDto = fixtureMonkeyValidation.giveMeOne(
+			ShipFishingPostRequest.Create.class);
+
+		ShipFishingPost givenShipFishingPost = ShipFishingPostConverter.fromShipFishPostsRequestCreate(givenRequestDto,
+			1L);
+
+		// When
+
+		// Then
+		assertThatThrownBy(() -> shipFishingPostServiceImpl.saveShipFishingPost(givenRequestDto, 1L))
+			.isInstanceOf(ShipException.class)
+			.hasFieldOrPropertyWithValue("shipErrorCode", ShipErrorCode.SHIP_NOT_FOUND)
+			.hasMessageContaining(ShipErrorCode.SHIP_NOT_FOUND.getMessage());
+
+	}
+
+	@Test
+	@DisplayName("선상 낚시 게시글 저장 [SHIP_MISMATCH_MEMBER_ID] [Service] - Fail")
+	void t03() {
+		// Given
+		ShipFishingPostRequest.Create givenRequestDto = fixtureMonkeyValidation.giveMeOne(
+			ShipFishingPostRequest.Create.class);
+
+		Ship givenShip = fixtureMonkeyBuilder.giveMeBuilder(Ship.class)
+			.set("memberId", 2L).sample();
+
+		ShipFishingPost givenShipFishingPost = ShipFishingPostConverter.fromShipFishPostsRequestCreate(givenRequestDto,
+			1L);
+
+		// When
+		when(shipRepository.findById(any(Long.class))).thenReturn(Optional.ofNullable(givenShip));
+
+		// Then
+		assertThatThrownBy(() -> shipFishingPostServiceImpl.saveShipFishingPost(givenRequestDto, 1L))
+			.isInstanceOf(ShipException.class)
+			.hasFieldOrPropertyWithValue("shipErrorCode", ShipErrorCode.SHIP_MISMATCH_MEMBER_ID)
+			.hasMessageContaining(ShipErrorCode.SHIP_MISMATCH_MEMBER_ID.getMessage());
+	}
+
+	@Test
+	@DisplayName("선상 낚시 게시글 상세 조회 [ShipFishingPostResponse.Detail] [Service] - Success")
+	void t04() {
 		// Given
 		ShipFishingPostResponse.Detail givenResponseDto = fixtureMonkeyValidation.giveMeBuilder(
 			ShipFishingPostResponse.Detail.class).set("shipFishingPostId", 1L).sample();
@@ -70,8 +125,8 @@ public class ShipFishingPostServiceTest extends BaseTest {
 	}
 
 	@Test
-	@DisplayName("선상 낚시 게시글 상세 조회 [Post Not Exists] [Service] - Fail")
-	void t03() {
+	@DisplayName("선상 낚시 게시글 상세 조회 [POSTS_NOT_FOUND] [Service] - Fail")
+	void t05() {
 		// Given
 
 		// When
@@ -79,6 +134,37 @@ public class ShipFishingPostServiceTest extends BaseTest {
 
 		// Then
 		assertThatThrownBy(() -> shipFishingPostServiceImpl.getShipFishingPost(1L))
+			.isInstanceOf(ShipFishingPostException.class)
+			.hasFieldOrPropertyWithValue("shipFishingPostErrorCode", ShipFishingPostErrorCode.POSTS_NOT_FOUND)
+			.hasMessageContaining(ShipFishingPostErrorCode.POSTS_NOT_FOUND.getMessage());
+	}
+
+	@Test
+	@DisplayName("선상 낚시 게시글 상세 조회 [ShipFishingPostResponse.DetailAll] [Service] - Success")
+	void t06() {
+		// Given
+		ShipFishingPostResponse.DetailAll givenDetailAll = fixtureMonkeyValidation.giveMeBuilder(
+			ShipFishingPostResponse.DetailAll.class).sample();
+
+		// When
+		when(shipFishingPostRepository.findDetailAllById(1L)).thenReturn(Optional.of(givenDetailAll));
+
+		ShipFishingPostResponse.DetailAll savedDetailAll = shipFishingPostServiceImpl.getShipFishingPostAll(1L);
+
+		// Then
+		assertThat(savedDetailAll).isEqualTo(givenDetailAll);
+	}
+
+	@Test
+	@DisplayName("선상 낚시 게시글 상세 조회 [ShipFishingPostResponse.DetailAll] [POSTS_NOT_FOUND] [Service] - Fail")
+	void t07() {
+		// Given
+
+		// When
+		when(shipFishingPostRepository.findDetailAllById(1L)).thenReturn(Optional.empty());
+
+		// Then
+		assertThatThrownBy(() -> shipFishingPostServiceImpl.getShipFishingPostAll(1L))
 			.isInstanceOf(ShipFishingPostException.class)
 			.hasFieldOrPropertyWithValue("shipFishingPostErrorCode", ShipFishingPostErrorCode.POSTS_NOT_FOUND)
 			.hasMessageContaining(ShipFishingPostErrorCode.POSTS_NOT_FOUND.getMessage());
