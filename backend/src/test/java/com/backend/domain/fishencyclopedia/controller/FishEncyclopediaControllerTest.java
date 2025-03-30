81961462916d7b22081271d2f4ff8af7120d8f84
Kim Dong O <kdo_1999@naver.com>
2025-03-30 21:02:28 +0900

    test: 미사용 변수 제거 (MIKKI-69)

--- backend/src/test/java/com/backend/domain/fishencyclopedia/controller/FishEncyclopediaControllerTest.java
+++ backend/src/test/java/com/backend/domain/fishencyclopedia/controller/FishEncyclopediaControllerTest.java
@@ -6,7 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -33,9 +32,6 @@ public class FishEncyclopediaControllerTest extends BaseTest {
 
 	@MockitoBean
 	private FishEncyclopediaService fishEncyclopediaService;
-
-	@InjectMocks
-	private FishEncyclopediaController fishEncyclopediaController;
 
 	@Autowired
 	private MockMvc mockMvc;
