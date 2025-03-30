a41087d78ee195e6e38fbca06e12a1af5acaf023
swjoon <jetkid1446@gmail.com>
2025-03-30 13:02:05 +0900

    feat: shipfishingpost 게시글 상세조회 메서드 (MIKKI-77) - repository

--- backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostQueryRepository.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostQueryRepository.java
@@ -1,7 +1,13 @@
 package com.backend.domain.shipfishingpost.repository;
 
+import static com.backend.domain.shipfishingpost.entity.QShipFishingPost.*;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
+import com.backend.domain.shipfishingpost.dto.response.QShipFishingPostResponse_Detail;
+import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +17,25 @@ import lombok.RequiredArgsConstructor;
 public class ShipFishingPostQueryRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
+
+	public Optional<ShipFishingPostResponse.Detail> findDetailById(final Long shipFishingPostId) {
+		ShipFishingPostResponse.Detail detail = jpaQueryFactory
+			.select(new QShipFishingPostResponse_Detail(
+				shipFishingPost.shipFishingPostId,
+				shipFishingPost.subject,
+				shipFishingPost.content,
+				shipFishingPost.price,
+				shipFishingPost.imageList,
+				shipFishingPost.startTime,
+				shipFishingPost.durationTime,
+				shipFishingPost.maxGuestCount,
+				shipFishingPost.reviewEverRate
+			))
+			.from(shipFishingPost)
+			.where(shipFishingPost.shipFishingPostId.eq(shipFishingPostId))
+			.fetchOne();
+
+		return Optional.ofNullable(detail);
+	}
 
 }
--- backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostRepositoryImpl.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostRepositoryImpl.java
@@ -1,7 +1,10 @@
 package com.backend.domain.shipfishingpost.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
+import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.backend.domain.shipfishingpost.entity.ShipFishingPost;
 
 import lombok.RequiredArgsConstructor;
@@ -14,9 +17,15 @@ public class ShipFishingPostRepositoryImpl implements ShipFishingPostRepository 
 	private final ShipFishingPostQueryRepository shipFishingPostQueryRepository;
 
 	@Override
-	public ShipFishingPost save(ShipFishingPost shipFishingPost) {
+	public ShipFishingPost save(final ShipFishingPost shipFishingPost) {
 
 		return shipFishingPostJpaRepository.save(shipFishingPost);
+	}
+
+	@Override
+	public Optional<ShipFishingPostResponse.Detail> findDetailById(final Long fishingPostId) {
+
+		return shipFishingPostQueryRepository.findDetailById(fishingPostId);
 	}
 
 }
