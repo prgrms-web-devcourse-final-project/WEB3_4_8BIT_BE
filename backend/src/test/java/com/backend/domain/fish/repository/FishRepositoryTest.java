eb855ed36218db5b8d196bd8b73510b98cf6a2fa
Kim Dong O <kdo_1999@naver.com>
2025-03-30 21:00:09 +0900

    test: 물고기 저장, 존재 여부 성공 테스트 추가 (MIKKI-69)

---
+++ backend/src/test/java/com/backend/domain/fish/repository/FishRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.backend.domain.fish.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.backend.domain.fish.entity.Fish;
+import com.backend.global.Util.BaseTest;
+import com.backend.global.config.JpaAuditingConfig;
+
+import com.navercorp.fixturemonkey.ArbitraryBuilder;
+
+@Import({FishRepositoryImpl.class, JpaAuditingConfig.class})
+@DataJpaTest
+class FishRepositoryTest extends BaseTest {
+
+	@Autowired
+	private FishRepository fishRepository;
+
+	final ArbitraryBuilder<Fish> arbitraryBuilder = fixtureMonkeyBuilder.giveMeBuilder(Fish.class)
+		.size("name", 1, 29)
+		.size("icon", 1, 49)
+		.size("spawnLocation", 1, 9);
+
+	@Test
+	@DisplayName("물고기 저장 [Repository] - Success")
+	void t01() {
+		// Given
+		Fish givenFish = arbitraryBuilder.set("fishId", null).sample();
+
+		// When
+		Fish savedFish = fishRepository.save(givenFish);
+
+		// Then
+		assertThat(savedFish.getFishId()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("물고기 존재 여부 조회 [Repository] - Success")
+	void t02() {
+		// Given
+		Fish gienFish = arbitraryBuilder.set("fishId", null).sample();
+		Fish savedFish = fishRepository.save(gienFish);
+
+		// When
+		boolean existsFish = fishRepository.existsById(savedFish.getFishId());
+
+		// Then
+		assertThat(existsFish).isTrue();
+	}
+}
