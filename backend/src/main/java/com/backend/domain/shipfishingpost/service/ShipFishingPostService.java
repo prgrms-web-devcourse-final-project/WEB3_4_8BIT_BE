273ff11bc73724312c7826b162bd3c4eec8e45d6
swjoon <jetkid1446@gmail.com>
2025-03-30 13:02:42 +0900

    feat: shipfishingpost 게시글 상세조회 메서드 (MIKKI-77) - service

--- backend/src/main/java/com/backend/domain/shipfishingpost/service/ShipFishingPostService.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/service/ShipFishingPostService.java
@@ -1,11 +1,12 @@
 package com.backend.domain.shipfishingpost.service;
 
 import com.backend.domain.shipfishingpost.dto.request.ShipFishingPostRequest;
+import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 
 public interface ShipFishingPostService {
 
 	/**
-	 * 선상 낚시 게시글 저장 메소드
+	 * 선상 낚시 게시글 저장 메서드
 	 *
 	 * @param requestDto {@link ShipFishingPostRequest.Create}
 	 * @return {@link Long ShipFishPostId} Long: 게시글 id
@@ -13,5 +14,15 @@ public interface ShipFishingPostService {
 	 * @author swjoon
 	 */
 	Long save(ShipFishingPostRequest.Create requestDto);
+
+	/**
+	 * 선상 낚시 게시글 상세 조회 메서드
+	 *
+	 * @param shipFishingPostId {@link Long}
+	 * @return {@link ShipFishingPostResponse.Detail}
+	 * @implSpec 선상 낚시 게시글 번호를 파라미터로 받고 조회한다.
+	 * @author swjoon
+	 */
+	ShipFishingPostResponse.Detail getShipFishingPost(Long shipFishingPostId);
 
 }
--- backend/src/main/java/com/backend/domain/shipfishingpost/service/ShipFishingPostServiceImpl.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/service/ShipFishingPostServiceImpl.java
@@ -1,14 +1,14 @@
 package com.backend.domain.shipfishingpost.service;
 
-import java.time.Duration;
-import java.time.LocalTime;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.backend.domain.shipfishingpost.converter.ShipFishingPostConverter;
 import com.backend.domain.shipfishingpost.dto.request.ShipFishingPostRequest;
+import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.backend.domain.shipfishingpost.entity.ShipFishingPost;
+import com.backend.domain.shipfishingpost.exception.ShipFishingPostErrorCode;
+import com.backend.domain.shipfishingpost.exception.ShipFishingPostException;
 import com.backend.domain.shipfishingpost.repository.ShipFishingPostRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -25,36 +25,24 @@ public class ShipFishingPostServiceImpl implements ShipFishingPostService {
 	@Transactional
 	public Long save(final ShipFishingPostRequest.Create requestDto) {
 
-		String durationTime = durationToString(requestDto.startTime(), requestDto.endTime());
+		ShipFishingPost shipFishingPost = ShipFishingPostConverter.fromShipFishPostsRequestCreate(requestDto);
 
-		ShipFishingPost entity = ShipFishingPostConverter.fromShipFishPostsRequestCreate(requestDto, durationTime);
+		// Todo : shipId로 등록 여부 & 본인 여부 검증
 
 		// Todo : 예약 불가날짜 관리 로직 필요
 		// requestDto.unavailableDates();
 
-		log.debug("Save ship fish posts: {}", entity);
+		// Todo : Fish 랑 중간 테이블 생성 후 추가 로직 구현
 
-		return shipFishingPostRepository.save(entity).getShipFishingPostId();
+		log.debug("Save ship fish posts: {}", shipFishingPost);
+
+		return shipFishingPostRepository.save(shipFishingPost).getShipFishingPostId();
 	}
 
-	/**
-	 * 시작시간, 마감시간 사이 기간 String 으로 변환 메서드
-	 *
-	 * @param startTime
-	 * @param endTime
-	 * @return {@link String duration}
-	 */
-	private String durationToString(final LocalTime startTime, final LocalTime endTime) {
-		Duration durationTime = Duration.between(startTime, endTime);
+	@Override
+	public ShipFishingPostResponse.Detail getShipFishingPost(final Long shipFishingPostId) {
 
-		if (durationTime.isNegative()) {
-			durationTime = durationTime.plusDays(1);
-		}
-
-		long totalMinutes = durationTime.toMinutes();
-		long hours = totalMinutes / 60;
-		long minutes = totalMinutes % 60;
-
-		return String.format("%02d:%02d", hours, minutes);
+		return shipFishingPostRepository.findDetailById(shipFishingPostId)
+			.orElseThrow(() -> new ShipFishingPostException(ShipFishingPostErrorCode.POSTS_NOT_FOUND));
 	}
 }
