e81bed93172044196fadb7ef3ebc36d702159e64
Hyeonsuk <vkdnjdjxor@naver.com>
2025-03-31 19:43:03 +0900

    feat: captain 상세보기 기능 구현(MIKKI-82)

--- backend/src/main/java/com/backend/domain/captain/repository/CaptainRepository.java
+++ backend/src/main/java/com/backend/domain/captain/repository/CaptainRepository.java
@@ -2,6 +2,7 @@ package com.backend.domain.captain.repository;
 
 import java.util.Optional;
 
+import com.backend.domain.captain.dto.Response.CaptainResponse;
 import com.backend.domain.captain.entity.Captain;
 
 public interface CaptainRepository {
@@ -15,13 +16,21 @@ public interface CaptainRepository {
 	 */
 	Captain save(final Captain captain);
 
-
 	/**
-	 * 선장 저장 메소드
+	 * 선장 조회 메소드
 	 *
 	 * @param captainId {@link Long}
 	 * @return {@link Optional<Captain>}
 	 * @implSpec Id로 선장 조회 메서드 입니다.
 	 */
 	Optional<Captain> findById(final Long captainId);
+
+	/**
+	 * 선장 상세 조회 메소드
+	 *
+	 * @param captainId {@link Long}
+	 * @return {@link Optional<CaptainResponse.Detail>}
+	 * @implSpec Id로 선장 상세 조회 메서드 입니다.
+	 */
+	Optional<CaptainResponse.Detail> findDetailById(final Long captainId);
 }
