48348c6178cf983cc1e091a5b770667e029a8b01
Hyeonsuk <vkdnjdjxor@naver.com>
2025-03-31 19:43:41 +0900

    feat: captain detail response 구현(MIKKI-82)

--- backend/src/main/java/com/backend/domain/captain/dto/Response/CaptainResponse.java
+++ backend/src/main/java/com/backend/domain/captain/dto/Response/CaptainResponse.java
@@ -1,6 +1,53 @@
 package com.backend.domain.captain.dto.Response;
 
+import java.util.List;
+
+import com.querydsl.core.annotations.QueryProjection;
+
+import lombok.Builder;
+
 public class CaptainResponse {
+
+	/**
+	 * {@code
+	 *   "memberId": 1,
+	 *   "email": "test@naver.com",
+	 *   "name": "루피",
+	 *   "nickname": "해적왕",
+	 *   "phone": "010-1234-5678",
+	 *   "profileImg": "http://example.com/profile.jpg",
+	 *   "description": "해적왕이 되고싶은 루피 입니다.",
+	 *   "shipLicenseNumber": "1-2019123456",
+	 *   "shipList": [1L, 2L, 3L]
+	 * }
+	 *
+	 * @param memberId 회원 ID
+	 * @param email 이메일
+	 * @param name 이름
+	 * @param nickname 닉네임
+	 * @param phone 전화번호
+	 * @param profileImg 프로필 이미지 URL
+	 * @param description 자기소개
+	 * @param shipLicenseNumber 선박 운전 면허 번호
+	 * @param shipList 선장 보유 배 Id 목록
+	 */
+	@Builder
+	public record Detail(
+		Long memberId,
+		String email,
+		String name,
+		String nickname,
+		String phone,
+		String profileImg,
+		String description,
+		String shipLicenseNumber,
+		List<Long> shipList
+	) {
+		@QueryProjection
+		public Detail {
+
+		}
+	}
 
 	/**
 	 * {@code
