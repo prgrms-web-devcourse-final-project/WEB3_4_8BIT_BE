4157328a1b79b93e5a5ee0c58c256adba57629dd
Kim Dong O <kdo_1999@naver.com>
2025-03-31 23:28:09 +0900

    test: 상세 조회 요청 Dto 성공 테스트 추가 (MIKKI-84)

---
+++ backend/src/test/java/com/backend/domain/fishencyclopedia/dto/request/FishEncyclopediaRequestTest.java
@@ -0,0 +1,76 @@
+package com.backend.domain.fishencyclopedia.dto.request;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FishEncyclopediaRequestTest {
+
+	@Test
+	@DisplayName("물고기 도감 상세 조회 요청 객체 [Size, Page is Null] [Dto] - Success")
+	void t01() {
+		// Given
+		Integer givenPage = null;
+		Integer givenSize = null;
+		String givenSort = null;
+		String givenOrder = null;
+
+		// When
+		FishEncyclopediaRequest.PageRequest requestDto = new FishEncyclopediaRequest.PageRequest(
+			givenPage,
+			givenSize,
+			givenSort,
+			givenOrder
+		);
+
+		// Then
+		assertThat(requestDto.page()).isEqualTo(0);
+		assertThat(requestDto.size()).isEqualTo(10);
+	}
+
+	@Test
+	@DisplayName("물고기 도감 상세 조회 요청 객체 [Size is Null] [Dto] - Success")
+	void t02() {
+		// Given
+		Integer givenPage = 10;
+		Integer givenSize = null;
+		String givenSort = null;
+		String givenOrder = null;
+
+		// When
+		FishEncyclopediaRequest.PageRequest requestDto = new FishEncyclopediaRequest.PageRequest(
+			givenPage,
+			givenSize,
+			givenSort,
+			givenOrder
+		);
+
+		// Then
+		assertThat(requestDto.page()).isEqualTo(10);
+		assertThat(requestDto.size()).isEqualTo(10);
+	}
+
+	@Test
+	@DisplayName("물고기 도감 상세 조회 요청 객체 [Page is Null] [Dto] - Success")
+	void t03() {
+		// Given
+		Integer givenPage = null;
+		Integer givenSize = 12;
+		String givenSort = null;
+		String givenOrder = null;
+
+		// When
+		FishEncyclopediaRequest.PageRequest requestDto = new FishEncyclopediaRequest.PageRequest(
+			givenPage,
+			givenSize,
+			givenSort,
+			givenOrder
+		);
+
+		// Then
+		assertThat(requestDto.page()).isEqualTo(0);
+		assertThat(requestDto.size()).isEqualTo(12);
+	}
+
+}
