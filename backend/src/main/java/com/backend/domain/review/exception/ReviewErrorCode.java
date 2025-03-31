01ef026f251897d259d51e01d2de20709b47446a
vdvhk12 <vdvhk12@gmail.com>
2025-04-01 00:11:44 +0900

    feat: 삭제 관련 에러코드 추가 (MIKKI-89)

--- backend/src/main/java/com/backend/domain/review/exception/ReviewErrorCode.java
+++ backend/src/main/java/com/backend/domain/review/exception/ReviewErrorCode.java
@@ -15,8 +15,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReviewErrorCode {
 
-	//선상 낚시 리뷰 에러 코드 600
-	DUPLICATE_REVIEW(HttpStatus.CONFLICT, 6001, "리뷰는 동일한 예약에 대해 한 번만 작성할 수 있습니다.");
+	//선상 낚시 리뷰 에러 코드 400
+	DUPLICATE_REVIEW(HttpStatus.CONFLICT, 4001, "리뷰는 동일한 예약에 대해 한 번만 작성할 수 있습니다."),
+	NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, 4002, "해당 리뷰를 찾을 수 없습니다."),
+	FORBIDDEN_REVIEW_DELETE(HttpStatus.FORBIDDEN, 4003, "리뷰 삭제 권한이 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final Integer code;
