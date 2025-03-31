e93b1e20e371b9c7aa1bf07d2e7ead15aa1fe7af
Kim Dong O <kdo_1999@naver.com>
2025-04-01 00:11:59 +0900

    feat: Swagger 어노테이션 추가

--- backend/src/main/java/com/backend/domain/fishencyclopedia/dto/request/FishEncyclopediaRequest.java
+++ backend/src/main/java/com/backend/domain/fishencyclopedia/dto/request/FishEncyclopediaRequest.java
@@ -27,22 +27,30 @@ public class FishEncyclopediaRequest {
 		@Schema(description = "물고기 길이", example = "10")
 		Integer length,
 
-		@Schema(description = "낚시 포인트 ID", example = "2")
-		@NotNull(message = "낚시 포인트 ID는 필수 항목입니다.")
-		Long fishPointId,
-
 		@Min(value = 1, message = "잡은 물고기 수는 1마리 이상이어야 합니다.")
 		@NotNull(message = "잡은 물고기 수는 필수 항목입니다.")
-		Integer count
+		@Schema(description = "잡은 물고기 수", example = "2")
+		Integer count,
+
+		@NotNull(message = "낚시 포인트 ID는 필수 항목입니다.")
+		@Schema(description = "낚시 포인트 ID", example = "2")
+		Long fishPointId
 	) {
 	}
 
 	public record PageRequest(
 		@Min(value = 0, message = "페이지 번호는 0 이상이어야 합니다.")
+		@Schema(description = "페이지 번호", example = "0")
 		Integer page,
+
 		@Min(value = 1, message = "페이지 사이즈는 0 이상이어야 합니다.")
+		@Schema(description = "페이지 사이즈", example = "10")
 		Integer size,
+
+		@Schema(description = "정렬 필드 [createdAt, count, length] (default createdAt)", example = "createdAt")
 		String sort,
+
+		@Schema(description = "정렬 필드 [asc, desc] (default desc)", example = "desc")
 		String order) {
 
 		public PageRequest {
