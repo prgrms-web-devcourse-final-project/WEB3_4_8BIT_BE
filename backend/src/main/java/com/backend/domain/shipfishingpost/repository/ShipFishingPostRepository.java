cdca23ff375fc4e20da01359b060e2e30172cd61
swjoon <jetkid1446@gmail.com>
2025-04-01 07:15:03 +0900

    feat: shipfishingpost 조회 페이징 메서드 (MIKKI-78) - Repository

--- backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostRepository.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostRepository.java
@@ -2,8 +2,12 @@ package com.backend.domain.shipfishingpost.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+
+import com.backend.domain.shipfishingpost.dto.request.ShipFishingPostRequest;
 import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.backend.domain.shipfishingpost.entity.ShipFishingPost;
+import com.backend.global.util.pageutil.Page;
 
 public interface ShipFishingPostRepository {
 
@@ -33,4 +37,15 @@ public interface ShipFishingPostRepository {
 	 * @implSpec 선상 낚시 게시글 ID를 받아서 조회 후 ShipFishingPostResponse.DetailAll 을 반환한다.
 	 */
 	Optional<ShipFishingPostResponse.DetailAll> findDetailAllById(final Long shipFishingPostId);
+
+	/**
+	 * 선상 낚시 게시글 조회 메서드
+	 *
+	 * @param search {@link ShipFishingPostRequest.Search}
+	 * @param pageable {@link Pageable}
+	 * @return {@link ShipFishingPostResponse.DetailPage}
+	 * @implSpec 여러 조건들과 검색 키워드를 통해 ShipFishingPostResponse.detailPage 을 페이징 하여 반환한다.
+	 */
+	Page<ShipFishingPostResponse.DetailPage> findAllBySearchAndCondition(final ShipFishingPostRequest.Search search,
+		final Pageable pageable);
 }
--- backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostRepositoryImpl.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/repository/ShipFishingPostRepositoryImpl.java
@@ -2,10 +2,13 @@ package com.backend.domain.shipfishingpost.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import com.backend.domain.shipfishingpost.dto.request.ShipFishingPostRequest;
 import com.backend.domain.shipfishingpost.dto.response.ShipFishingPostResponse;
 import com.backend.domain.shipfishingpost.entity.ShipFishingPost;
+import com.backend.global.util.pageutil.Page;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,5 +35,13 @@ public class ShipFishingPostRepositoryImpl implements ShipFishingPostRepository 
 	public Optional<ShipFishingPostResponse.DetailAll> findDetailAllById(final Long shipFishingPostId) {
 
 		return shipFishingPostQueryRepository.findDetailAllById(shipFishingPostId);
+	}
+
+	@Override
+	public Page<ShipFishingPostResponse.DetailPage> findAllBySearchAndCondition(
+		final ShipFishingPostRequest.Search search,
+		final Pageable pageable) {
+
+		return shipFishingPostQueryRepository.findDetailPage(search, pageable);
 	}
 }
