5f5a9e4120119dda3d6d43c2b5a925bd8746018e
Kim Dong O <kdo_1999@naver.com>
2025-04-01 00:28:37 +0900

    refactor: 물고기 도감 에러코드 수정 (MIKKI-84)

--- backend/src/main/java/com/backend/domain/fishencyclopedia/exception/FishEncyclopediaErrorCode.java
+++ backend/src/main/java/com/backend/domain/fishencyclopedia/exception/FishEncyclopediaErrorCode.java
@@ -9,9 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FishEncyclopediaErrorCode {
 
-	//물고기 도감 에러 코드 100
-	NOT_EXISTS_FISH(HttpStatus.NOT_FOUND, 1001, "물고기가 존재하지 않습니다."),
-	NOT_EXISTS_FISH_POINT(HttpStatus.NOT_FOUND, 1002, "낚시 포인트가 존재하지 않습니다.");
+	// 물고기 도감 에러 코드 900
+	NOT_EXISTS_FISH(HttpStatus.NOT_FOUND, 9001, "물고기가 존재하지 않습니다."),
+	NOT_EXISTS_FISH_POINT(HttpStatus.NOT_FOUND, 9002, "낚시 포인트가 존재하지 않습니다.");
 
 	private final HttpStatus httpStatus;
 	private final int code;
