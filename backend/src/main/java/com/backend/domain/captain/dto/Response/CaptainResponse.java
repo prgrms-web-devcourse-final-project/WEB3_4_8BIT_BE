b2f99515817d8c7061558792ff28dc2350bece56
Hyeonsuk <vkdnjdjxor@naver.com>
2025-03-31 15:27:05 +0900

    feat: captain request, response 구현(MIKKI-81)

---
+++ backend/src/main/java/com/backend/domain/captain/dto/Response/CaptainResponse.java
@@ -0,0 +1,30 @@
+package com.backend.domain.captain.dto.Response;
+
+public class CaptainResponse {
+
+	/**
+	 * {@code
+	 *   "nickname": "해적왕",
+	 *   "profileImg": "http://example.com/profile.jpg",
+	 *   "role": "CAPTAIN",
+	 *   "isAddInfo": "true"
+	 * }
+	 *
+	 * @param nickname 선장 닉네임
+	 * @param profileImg 프로필 이미지 URL
+	 * @param role 사용자 역할 (CAPTAIN)
+	 * @param isAddInfo 추가정보 입력 여부
+	 */
+	//TODO 추후 로직에 따라 적용 여부 판별
+
+	//
+	// @Builder
+	// public record Login(
+	// 	String nickname,
+	// 	String profileImg,
+	// 	String role,
+	// 	boolean isAddInfo
+	// ) {
+	//
+	// }
+}
