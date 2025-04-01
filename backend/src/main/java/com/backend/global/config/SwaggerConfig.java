b108d259d40f88c793907fa32e414a1adac809a0
강정수 <todemd12@gmail.com>
2025-03-31 23:33:44 -0700

    Update SwaggerConfig.java

--- backend/src/main/java/com/backend/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/backend/global/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
