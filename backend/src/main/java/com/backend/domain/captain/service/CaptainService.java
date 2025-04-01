02a77a3c8ec5b47030d30b9e67136a9dd2f81bfe
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-01 12:12:47 +0900

    feat: captain shipList update 기능 구현(MIKKI-91)

--- backend/src/main/java/com/backend/domain/captain/service/CaptainService.java
+++ backend/src/main/java/com/backend/domain/captain/service/CaptainService.java
@@ -18,7 +18,7 @@ public interface CaptainService {
 
 
 	/**
-	 * 선장 저장 메소드
+	 * 선장 상세 조회 메소드
 	 *
 	 * @param captainId 선장 Id
 	 * @return {@link CaptainResponse.Detail} 선장 상세정보 응답 데이터
@@ -26,4 +26,15 @@ public interface CaptainService {
 	 * @author Janghyeonsuk
 	 */
 	CaptainResponse.Detail getCaptainDetail(final Long captainId);
+
+	/**
+	 * 선장 배 리스트 수정 메소드
+	 *
+	 * @param captainId 선장 Id
+	 * @param requestDto 배 리스트 포함된 dto
+	 * @return {@link Long} 업데이트 된 선장 Id
+	 * @implSpec 새로운 배 리스트를 받고 선장 정보 업데이트
+	 * @author Janghyeonsuk
+	 */
+	Long updateCaptainShipList(final Long captainId, final CaptainRequest.Update requestDto);
 }
