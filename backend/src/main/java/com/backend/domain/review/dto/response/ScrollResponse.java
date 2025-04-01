4200d5757a2d31c9bdc79577abf7c5bc66ccff94
vdvhk12 <vdvhk12@gmail.com>
2025-04-01 12:40:14 +0900

    refactor: ScrollResponse 필드 변경 (MIKKI-83)

--- backend/src/main/java/com/backend/domain/review/dto/response/ScrollResponse.java
+++ backend/src/main/java/com/backend/domain/review/dto/response/ScrollResponse.java
@@ -4,9 +4,23 @@ import java.util.List;
 
 import org.springframework.data.domain.Slice;
 
-public record ScrollResponse<T>(List<T> content, boolean hasNext) {
+public record ScrollResponse<T>(
+	List<T> content,
+	int pageNumber,
+	int pageSize,
+	int numberOfElements,
+	boolean first,
+	boolean last
+) {
 
 	public static <T> ScrollResponse<T> from(Slice<T> slice) {
-		return new ScrollResponse<>(slice.getContent(), slice.hasNext());
+		return new ScrollResponse<>(
+			slice.getContent(),
+			slice.getNumber(),
+			slice.getSize(),
+			slice.getNumberOfElements(),
+			slice.isFirst(),
+			slice.isLast()
+		);
 	}
 }
