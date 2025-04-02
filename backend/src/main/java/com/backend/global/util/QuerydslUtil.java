d093b4ab39af70538bd9bb52d4a226b270dcfc23
Kim Dong O <kdo_1999@naver.com>
2025-04-03 00:57:19 +0900

    comment: 주석 추가 (MIKKI-99)

--- backend/src/main/java/com/backend/global/util/QuerydslUtil.java
+++ backend/src/main/java/com/backend/global/util/QuerydslUtil.java
@@ -11,6 +11,12 @@ import com.querydsl.core.types.dsl.NumberPath;
 
 public class QuerydslUtil {
 
+	/**
+	 * Order 객체로 변환하여 반환합니다.
+	 *
+	 * @param pageRequestDto
+	 * @return {@link Order} 기본 값 DESC
+	 */
 	public static Order getOrder(GlobalRequest.CursorRequest pageRequestDto) {
 		// 기본 정렬은 DESC로 동작
 		return Order.ASC.toString().equalsIgnoreCase(pageRequestDto.order()) ?
@@ -20,6 +26,7 @@ public class QuerydslUtil {
 
 	/**
 	 * 커서 방식 Where 조건절 생성 메소드 입니다.
+	 * <p>Integer 타입의 필드만 사용 가능합니다.</p>
 	 *
 	 * @param idField 기본키 필드
 	 * @param idFieldValue 기본키 필드 값
@@ -41,6 +48,18 @@ public class QuerydslUtil {
 			.or(order.equals(Order.DESC) ? sortField.lt(sortFieldValue) : sortField.gt(sortFieldValue));
 	}
 
+	/**
+	 * 커서 방식 Where 조건절 생성 메소드 입니다.
+	 * <p>ZonedDateTime 타입의 필드만 사용 가능합니다.</p>
+	 *
+	 * @param idField 기본키 필드
+	 * @param idFieldValue 기본키 필드 값
+	 * @param sortField 정렬 대상 필드
+	 * @param sortFieldValue 정렬 대상 필드 값
+	 * @param order 정렬 순서
+	 *
+	 * @return {@link BooleanExpression}
+	 */
 	public static BooleanExpression createFieldPredicate(
 		NumberPath<Long> idField,
 		Long idFieldValue,
