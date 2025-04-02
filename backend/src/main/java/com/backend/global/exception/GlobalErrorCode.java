5dcd816d7e7df2f75e8fa1296896c688b2d21535
Kim Dong O <kdo_1999@naver.com>
2025-04-03 00:58:36 +0900

    feat: 커서 페이징 로직에 변환 예외 발생시 사용할 에러코드 추가 (MIKKI-99)

--- backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
@@ -18,7 +18,9 @@ public enum GlobalErrorCode implements ErrorCode {
 	//공통 서버 에러 코드 500
 	NOT_VALID(HttpStatus.BAD_REQUEST, 5001, "요청하신 유효성 검증에 실패하였습니다."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5002, "서버 내부 오류가 발생하였습니다."),
-	WRONG_SORT_CONDITION(HttpStatus.BAD_REQUEST, 5003, "지원하지 않는 정렬 항목입니다.");
+	WRONG_SORT_CONDITION(HttpStatus.BAD_REQUEST, 5003, "지원하지 않는 정렬 항목입니다."),
+	REPOSITORY_FORMAT_PARSE_ERROR(HttpStatus.BAD_REQUEST, 5004, "파라미터 타입이 잘못됐습니다.");
+;
 
 	private final HttpStatus httpStatus;
 	private final Integer code;
