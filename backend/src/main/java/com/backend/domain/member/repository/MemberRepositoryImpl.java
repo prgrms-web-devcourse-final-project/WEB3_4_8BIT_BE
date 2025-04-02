4480db0e90b4f5e18e335551c988a22a2b242a5e
강정수 <emdtod12@naver.com>
2025-04-01 20:26:07 -0700

    feat : querydsl로 detial 찾아오도록 기능 구현 (MIKKI-100)

--- backend/src/main/java/com/backend/domain/member/repository/MemberRepositoryImpl.java
+++ backend/src/main/java/com/backend/domain/member/repository/MemberRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.backend.domain.member.dto.MemberResponse;
 import com.backend.domain.member.entity.Member;
 
 import lombok.RequiredArgsConstructor;
@@ -15,17 +16,22 @@ public class MemberRepositoryImpl implements MemberRepository {
 	private final MemberQueryRepository memberQueryRepository;
 
 	@Override
-	public Member save(Member member){
+	public Member save(Member member) {
 		return memberJpaRepository.save(member);
 	}
 
 	@Override
-	public Optional<Member> findByPhone(String phone){
+	public Optional<Member> findByPhone(String phone) {
 		return memberJpaRepository.findByPhone(phone);
 	}
 
 	@Override
 	public Optional<Member> findById(Long id) {
 		return memberJpaRepository.findById(id);
+	}
+
+	@Override
+	public Optional<MemberResponse.Detail> findDetailById(Long memberId) {
+		return memberQueryRepository.findDetailById(memberId);
 	}
 }
