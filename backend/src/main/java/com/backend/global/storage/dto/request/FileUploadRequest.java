361ef850dbc3883e8100b2990ab3c350765d6834
vdvhk12 <vdvhk12@gmail.com>
2025-04-02 18:49:05 +0900

    feat: 업로드 완료 요청 DTO 추가 (MIKKI-61)

--- backend/src/main/java/com/backend/global/storage/dto/request/FileUploadRequest.java
+++ backend/src/main/java/com/backend/global/storage/dto/request/FileUploadRequest.java
@@ -13,6 +13,7 @@ import jakarta.validation.constraints.Size;
 
 public class FileUploadRequest {
 
+	@Schema(description = "업로드할 단일 파일 정보")
 	public record UploadFile(
 		@NotBlank(message = "파일명은 필수 항목입니다.")
 		@Schema(description = "파일명", example = "IMG_3687.jpeg")
@@ -29,6 +30,7 @@ public class FileUploadRequest {
 		String contentType
 	) {}
 
+	@Schema(description = "Presigned URL 요청 DTO")
 	public record Request (
 		@NotBlank(message = "도메인은 필수 항목입니다.")
 		@Schema(description = "도메인명", example = "profile")
@@ -38,5 +40,12 @@ public class FileUploadRequest {
 		@Size(max = 10, message = "파일은 최대 10개까지 업로드할 수 있습니다.")
 		@Valid
 		List<UploadFile> uploadFileList
+	) {}
+
+	@Schema(description = "파일 업로드 완료 요청 DTO")
+	public record Complete(
+		@NotEmpty(message = "파일 ID 리스트는 필수 항목입니다.")
+		@Schema(description = "업로드 완료된 파일의 ID 리스트", example = "[101, 102, 103]")
+		List<Long> fileIdList
 	) {}
 }
