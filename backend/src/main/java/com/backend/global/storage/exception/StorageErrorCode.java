5b2c4e93420bcf00968f7d1e45c24dac7ad3ff84
vdvhk12 <vdvhk12@gmail.com>
2025-04-03 09:59:55 +0900

    refactor: 에러코드 httpStatus 상태 값 수정 (MIKKI-61)

--- backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
+++ backend/src/main/java/com/backend/global/storage/exception/StorageErrorCode.java
@@ -22,7 +22,7 @@ public enum StorageErrorCode implements ErrorCode {
 	FILE_NOT_FOUND(HttpStatus.NOT_FOUND, 5002, "존재하지 않는 파일 ID가 포함되어 있습니다."),
 	ALREADY_UPLOADED_FILE(HttpStatus.BAD_REQUEST, 5003, "이미 업로드 완료된 파일이 포함되어 있습니다."),
 	NO_FILE_ACCESS(HttpStatus.FORBIDDEN, 5004, "해당 파일에 대한 접근 권한이 없습니다."),
-	S3_FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, 5005, "S3에 파일이 존재하지 않습니다."),
+	S3_FILE_NOT_FOUND(HttpStatus.NOT_FOUND, 5005, "S3에 파일이 존재하지 않습니다."),
 	S3_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 5006, "S3에서 파일 삭제 중 오류가 발생했습니다.")
 	;
 
