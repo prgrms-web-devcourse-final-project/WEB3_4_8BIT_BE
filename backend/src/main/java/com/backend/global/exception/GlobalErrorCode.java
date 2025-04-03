b7143afd36d579c7785f4bf4c30413f8398e8087
swjoon <jetkid1446@gmail.com>
2025-04-04 00:04:25 +0900

    fix: merge conflict 해결 (MIKKI-96)

--- backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
@@ -19,8 +19,8 @@ public enum GlobalErrorCode implements ErrorCode {
 	NOT_VALID(HttpStatus.BAD_REQUEST, 5001, "요청하신 유효성 검증에 실패하였습니다."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5002, "서버 내부 오류가 발생하였습니다."),
 	WRONG_SORT_CONDITION(HttpStatus.BAD_REQUEST, 5003, "지원하지 않는 정렬 항목입니다."),
-	REPOSITORY_FORMAT_PARSE_ERROR(HttpStatus.BAD_REQUEST, 5004, "파라미터 타입이 잘못됐습니다.");
-;
+	REPOSITORY_FORMAT_PARSE_ERROR(HttpStatus.BAD_REQUEST, 5004, "파라미터 타입이 잘못됐습니다."),
+	DATA_INTEGRITY_VIOLATION(HttpStatus.CONFLICT, 5005, "데이터 무결성 오류가 발생했습니다. (중복 데이터 등)");
 
 	private final HttpStatus httpStatus;
 	private final Integer code;
