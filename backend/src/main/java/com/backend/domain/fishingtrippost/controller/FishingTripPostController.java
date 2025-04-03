7fd164ae2d8bfa99b50c0979a4cd3b2453760aeb
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-03 17:16:23 +0900

    feat: fishingtrippost controller 기능 구현(MIKKI-45)

---
+++ backend/src/main/java/com/backend/domain/fishingtrippost/controller/FishingTripPostController.java
@@ -0,0 +1,43 @@
+package com.backend.domain.fishingtrippost.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.domain.fishingtrippost.dto.request.FishingTripPostRequest;
+import com.backend.domain.fishingtrippost.service.FishingTripPostService;
+import com.backend.global.auth.oauth2.CustomOAuth2User;
+import com.backend.global.dto.response.GenericResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "동출 모집 API")
+@RequestMapping("/api/v1/fishing-trip-post")
+public class FishingTripPostController {
+
+	private final FishingTripPostService fishingTripPostService;
+
+	@PostMapping
+	@Operation(summary = "동출 모집 게시글 생성", description = "로그인한 사용자가 동출 모집 게시글 작성시 사용하는 API")
+	public ResponseEntity<GenericResponse<Long>> createFishingTripPost(
+		@AuthenticationPrincipal final CustomOAuth2User user,
+		@RequestBody @Valid final FishingTripPostRequest.Create requestDto
+	) {
+
+		Long saveFishingTripPostId = fishingTripPostService.createFishingTripPost(user.getId(), requestDto);
+
+		return ResponseEntity.created(URI.create(saveFishingTripPostId.toString()))
+			.body(GenericResponse.of(true));
+	}
+
+}
