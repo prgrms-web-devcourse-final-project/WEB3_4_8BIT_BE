c9d224402fd5a8268ab00d03e9741d1ade04fc5f
swjoon <jetkid1446@gmail.com>
2025-04-04 00:20:52 +0900

    feat: 예약 일자 ErrorCode 추가 (MIKKI-96)

--- backend/src/main/java/com/backend/domain/reservation/exception/ReservationErrorCode.java
+++ backend/src/main/java/com/backend/domain/reservation/exception/ReservationErrorCode.java
@@ -11,7 +11,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReservationErrorCode implements ErrorCode {
 
-	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, 11001, "예약 정보가 존재 하지 않습니다.");
+	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, 11001, "예약 정보가 존재 하지 않습니다."),
+	WRONG_PRICE_VALUE(HttpStatus.BAD_REQUEST, 11002, "예약 금액이 일치하지 않습니다."),
+	NOT_AVAILABLE_DATE_RESERVATION(HttpStatus.CONFLICT, 11003, "예약 가능한 일자가 아닙니다."),
+	NOT_AVAILABLE_END_RESERVATION(HttpStatus.CONFLICT, 11004, "예약 정원 초과 입니다.");
 
 	private final HttpStatus httpStatus;
 	private final Integer code;
