a2e62baffd4e72334494f8ed43df3d4f5c101f0a
강정수 <emdtod12@naver.com>
2025-04-04 08:28:53 -0700

    style : findDetailById 주석 @implSpec 추가 (MIKKI-110)

--- backend/src/main/java/com/backend/domain/fishingtrippost/repository/FishingTripPostRepository.java
+++ backend/src/main/java/com/backend/domain/fishingtrippost/repository/FishingTripPostRepository.java
@@ -33,9 +33,10 @@ public interface FishingTripPostRepository {
 	 * <p>조회된 정보는 응답 DTO {@link FishingTripPostResponse.Detail} 로 변환되어 반환되며,
 	 * 내부적으로 포맷팅된 날짜 및 인원 수 형식의 문자열을 포함합니다.</p>
 	 *
+	 * @implSpec 이 메서드는 예외를 던지지 않고, 결과가 없을 경우 {@link Optional#empty()}를 반환해야 합니다.
+	 *
 	 * @param fishingTripPostId 조회할 게시글의 고유 ID
-	 * @return 해당 ID의 게시글 상세 정보가 존재할 경우 {@link FishingTripPostResponse.Detail}을 포함한 Optional,
-	 *         존재하지 않으면 {@link Optional#empty()}
+	 * @return 게시글 상세 정보 Optional
 	 */
 
 	Optional<FishingTripPostResponse.Detail> findDetailById(final Long fishingTripPostId);
