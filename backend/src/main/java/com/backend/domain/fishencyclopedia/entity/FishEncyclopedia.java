314c7a4f1c7341539b521cd82426bb37d1c54054
Kim Dong O <kdo_1999@naver.com>
2025-04-04 10:06:00 +0900

    refactor: 인덱스 추가 (MIKKI-99)

--- backend/src/main/java/com/backend/domain/fishencyclopedia/entity/FishEncyclopedia.java
+++ backend/src/main/java/com/backend/domain/fishencyclopedia/entity/FishEncyclopedia.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,7 +15,14 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
-@Table(name = "fish_encyclopedias")
+@Table(
+	name = "fish_encyclopedias",
+	indexes = {
+		@Index(name = "idx_fish_encyclopedias_01", columnList = ("fish_id, member_id, created_at, fish_encyclopedia_id")),
+		@Index(name = "idx_fish_encyclopedias_02", columnList = ("fish_id, member_id, count, fish_encyclopedia_id")),
+		@Index(name = "idx_fish_encyclopedias_03", columnList = ("fish_id, member_id, length, fish_encyclopedia_id"))
+	}
+)
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
