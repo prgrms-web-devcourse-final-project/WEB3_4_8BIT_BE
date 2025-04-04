3add0900a86ba77c9ed05f441c2a0ab45b0c03cd
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-04 12:59:39 +0900

    feat: fishingtrippost table 명 추가(MIKKI-109)

--- backend/src/main/java/com/backend/domain/fishingtrippost/entity/FishingTripPost.java
+++ backend/src/main/java/com/backend/domain/fishingtrippost/entity/FishingTripPost.java
@@ -15,12 +15,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+@Table(name = "fishing_trip_posts")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
