4c249d6a0cf35a88eb87eb145d4f5db6344e4f6b
Kim Dong O <kdo_1999@naver.com>
2025-04-06 01:07:22 +0900

    refactor: validation 메세지 수정 (MIKKI-122)

--- backend/src/main/java/com/backend/domain/fish/dto/FishRequest.java
+++ backend/src/main/java/com/backend/domain/fish/dto/FishRequest.java
@@ -15,7 +15,7 @@ public class FishRequest {
 	 * @author Kim Dong O
 	 */
 	public record Popular(
-		@Min(value = 1, message = "사이즈는 1개 이상이어야 합니다. ")
+		@Min(value = 1, message = "사이즈는 1개 이상이어야 합니다.")
 		@Max(value = 10, message = "사이즈는 10개 이하여야 합니다.")
 		@NotNull(message = "사이즈는 필수 항목입니다.")
 		@Schema(description = "가져올 데이터 개수", example = "4")
