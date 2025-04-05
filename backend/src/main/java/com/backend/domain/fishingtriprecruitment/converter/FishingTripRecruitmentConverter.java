8c73833e3b5e98b0a6323da188aaf9697069d330
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-06 00:06:59 +0900

    feat: 동출 모집 신청 dto, converter 개발(MIKKI-120)

---
+++ backend/src/main/java/com/backend/domain/fishingtriprecruitment/converter/FishingTripRecruitmentConverter.java
@@ -0,0 +1,29 @@
+package com.backend.domain.fishingtriprecruitment.converter;
+
+import com.backend.domain.fishingtriprecruitment.domain.RecruitmentStatus;
+import com.backend.domain.fishingtriprecruitment.dto.request.FishingTripRecruitmentRequest;
+import com.backend.domain.fishingtriprecruitment.entity.FishingTripRecruitment;
+
+public class FishingTripRecruitmentConverter {
+
+	/**
+	 * 로그인한 멤버가 동출 모집 신청 Dto를 Entity로 변환 메서드
+	 *
+	 * @param memberId   {@link Long}
+	 * @param requestDto {@link FishingTripRecruitmentRequest.Create}
+	 * @return {@link FishingTripRecruitment}
+	 */
+
+	public static FishingTripRecruitment fromFishingTripRecruitmentCreate(
+		final Long memberId,
+		final FishingTripRecruitmentRequest.Create requestDto
+	) {
+		return FishingTripRecruitment.builder()
+			.fishingLevel(requestDto.fishingLevel())
+			.introduction(requestDto.introduction())
+			.recruitmentStatus(RecruitmentStatus.PENDING) // 신청시 "대기"로 기본값
+			.fishingTripPostId(requestDto.fishingTripPostId())
+			.memberId(memberId)
+			.build();
+	}
+}
