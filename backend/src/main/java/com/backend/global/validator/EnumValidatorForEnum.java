9e5af78353c426217d1952ff5471020b4b8974b2
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-06 03:06:05 +0900

    feat: 영어, 한글 값 모두 사용할 수 있도록 변경(MIKKI120)

--- backend/src/main/java/com/backend/global/validator/EnumValidatorForEnum.java
+++ backend/src/main/java/com/backend/global/validator/EnumValidatorForEnum.java
@@ -1,10 +1,13 @@
 package com.backend.global.validator;
 
-import java.util.Arrays;
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class EnumValidatorForEnum implements ConstraintValidator<ValidEnum, Enum<?>> {
 
 	private ValidEnum validEnum;
@@ -20,11 +23,39 @@ public class EnumValidatorForEnum implements ConstraintValidator<ValidEnum, Enum
 			return validEnum.nullable();
 		}
 
-		Object[] enumConstants = validEnum.enumClass().getEnumConstants();
-		if (enumConstants == null) {
-			return false;
+		Class<? extends Enum<?>> enumClass = validEnum.enumClass();
+		Object[] enumConstants = enumClass.getEnumConstants();
+
+		Method getDisplayNameMethod = null;
+		try {
+			getDisplayNameMethod = enumClass.getMethod("getDisplayName");
+		} catch (NoSuchMethodException e) {
+			log.debug("[EnumValidator] '{}' enum getDisplayName() 메서드가 없어 무시합니다.",
+				enumClass.getSimpleName());
 		}
 
-		return Arrays.asList(enumConstants).contains(value);
+		for (Object constant : enumConstants) {
+			Enum<?> enumConstant = (Enum<?>)constant;
+
+			// 기본적으로 name() 으로 비교
+			if (enumConstant.name().equals(value.name())) {
+				return true;
+			}
+
+			// getDisplayName() 이 존재하면 비교
+			if (getDisplayNameMethod != null) {
+				try {
+					Object displayName = getDisplayNameMethod.invoke(enumConstant);
+					if (Objects.equals(displayName, value.toString())) {
+						return true;
+					}
+				} catch (Exception e) {
+					log.warn("[EnumValidator] '{}' enum getDisplayName() 호출 중 예외 발생",
+						enumClass.getSimpleName(), e);
+				}
+			}
+		}
+
+		return false;
 	}
 }
--- backend/src/main/java/com/backend/global/validator/EnumValidatorForString.java
+++ backend/src/main/java/com/backend/global/validator/EnumValidatorForString.java
@@ -1,10 +1,12 @@
 package com.backend.global.validator;
 
-import java.util.Arrays;
+import java.lang.reflect.Method;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class EnumValidatorForString implements ConstraintValidator<ValidEnum, String> {
 
 	private ValidEnum validEnum;
@@ -25,8 +27,32 @@ public class EnumValidatorForString implements ConstraintValidator<ValidEnum, St
 			return false;
 		}
 
-		return Arrays.stream(enumConstants)
-			.map(enumConst -> ((Enum<?>)enumConst).name())
-			.anyMatch(enumName -> enumName.equalsIgnoreCase(value));
+		Method getDisplayNameMethod = null;
+		try {
+			getDisplayNameMethod = validEnum.enumClass().getMethod("getDisplayName");
+		} catch (NoSuchMethodException e) {
+			log.debug("[EnumValidator] '{}' enum getDisplayName() 메서드가 없어 무시합니다.",
+				validEnum.enumClass().getSimpleName());
+		}
+
+		for (Object constant : enumConstants) {
+			Enum<?> enumValue = (Enum<?>)constant;
+			if (enumValue.name().equalsIgnoreCase(value)) {
+				return true;
+			}
+			if (getDisplayNameMethod != null) {
+				try {
+					Object displayName = getDisplayNameMethod.invoke(constant);
+					if (value.equalsIgnoreCase(displayName.toString())) {
+						return true;
+					}
+				} catch (Exception e) {
+					log.warn("[EnumValidator] '{}' enum getDisplayName() 호출 중 예외 발생",
+						validEnum.enumClass().getSimpleName(), e);
+				}
+			}
+		}
+
+		return false;
 	}
 }
