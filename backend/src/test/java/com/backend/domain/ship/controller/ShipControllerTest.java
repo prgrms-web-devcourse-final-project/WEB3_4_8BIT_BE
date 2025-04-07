51b0738d59421ecb79400a7a96f07810cfe868ca
Kim Dong O <kdo_1999@naver.com>
2025-04-07 23:12:02 +0900

    test: 선박 저장 실패 테스트 추가 (MIKKI-136)

--- backend/src/test/java/com/backend/domain/ship/controller/ShipControllerTest.java
+++ backend/src/test/java/com/backend/domain/ship/controller/ShipControllerTest.java
@@ -19,10 +19,12 @@ import org.springframework.test.web.servlet.ResultActions;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
 
+import com.backend.domain.ship.domain.RestroomType;
 import com.backend.domain.ship.dto.request.ShipRequest;
 import com.backend.domain.ship.service.ShipService;
 import com.backend.global.auth.WithMockCustomUser;
 import com.backend.global.config.TestSecurityConfig;
+import com.backend.global.exception.GlobalErrorCode;
 import com.backend.global.util.BaseTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -74,5 +76,403 @@ class ShipControllerTest extends BaseTest {
 			.andExpect(status().isCreated())
 			.andExpect(header().exists("Location"))
 			.andExpect(header().string("Location", shipId.toString()));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [ShipName - Size] [Controller] - Fail")
+	@WithMockCustomUser
+	void t02() throws Exception {
+		// Given
+		Long shipId = 1L;
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("shipName", "oiesfajoiejfioewafjoijwaoiefjwaoiefjiowjafiowjaofijwoaiefowaiefjoiwafjowaif")
+			.sample();
+
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("shipName"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 이름은 30자 이하여야 합니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [ShipName - NotBlank] [Controller] - Fail")
+	@WithMockCustomUser
+	void t03() throws Exception {
+		// Given
+		Long shipId = 1L;
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("shipName", "")
+			.sample();
+
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("shipName"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 이름은 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [ShipNumber - Size] [Controller] - Fail")
+	@WithMockCustomUser
+	void t04() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("shipNumber", "oiesfajoiejfioewafjoijwaoiefjwaoiefjiowjafiowjaofijwoaiefowaiefjoiwafjowaif")
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("shipNumber"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 번호는 30자 이하여야 합니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [ShipNumber - NotBlank] [Controller] - Fail")
+	@WithMockCustomUser
+	void t05() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("shipNumber", "")
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("shipNumber"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 번호는 필수 항목입니다,"))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [DeparturePort - Size] [Controller] - Fail")
+	@WithMockCustomUser
+	void t06() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("departurePort", "oiesfajoiejfioewafjoijwaoiefjwaoiefjiowjafiowjaofijwoaiefowaiefjoiwafjowaif")
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("departurePort"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 이름은 30자 이하여야 합니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [DeparturePort - NotBlank] [Controller] - Fail")
+	@WithMockCustomUser
+	void t07() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("departurePort", "")
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("departurePort"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 이름은 필수 항목입니다,"))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [PassengerCapacity - Min] [Controller] - Fail")
+	@WithMockCustomUser
+	void t08() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("passengerCapacity", 0)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("passengerCapacity"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 정원은 1명 이상이어야 합니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [PassengerCapacity - Max] [Controller] - Fail")
+	@WithMockCustomUser
+	void t09() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("passengerCapacity", 101)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("passengerCapacity"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 정원은 100명 이하여야 합니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [PassengerCapacity - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t10() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("passengerCapacity", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("passengerCapacity"))
+			.andExpect(jsonPath("$.data[0].reason").value("선박 정원은 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [RestroomType - ValidEnum] [Controller] - Fail")
+	@WithMockCustomUser
+	void t11() throws Exception {
+		// Given
+		String invalidJson = objectMapper
+			.writeValueAsString(createArbitraryBuilder.set("restroomType", RestroomType.PUBLIC).sample())
+			.replace("\"공용 화장실\"", "\"TEST\"");
+
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(invalidJson));
+
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("restroomType"))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [LoungeArea - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t12() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("loungeArea", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("loungeArea"))
+			.andExpect(jsonPath("$.data[0].reason").value("휴게 공간 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [KitchenFacility - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t13() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("kitchenFacility", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("kitchenFacility"))
+			.andExpect(jsonPath("$.data[0].reason").value("조리 시설 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [FishingChair - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t14() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("fishingChair", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("fishingChair"))
+			.andExpect(jsonPath("$.data[0].reason").value("낚시 의자 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [PassengerInsurance - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t15() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("passengerInsurance", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("passengerInsurance"))
+			.andExpect(jsonPath("$.data[0].reason").value("승객 보험 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [FishingGearRental - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t16() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("fishingGearRental", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("fishingGearRental"))
+			.andExpect(jsonPath("$.data[0].reason").value("장비 대여 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [MealProvided - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t17() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("mealProvided", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("mealProvided"))
+			.andExpect(jsonPath("$.data[0].reason").value("식사 제공 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
+	}
+
+	@Test
+	@DisplayName("선박 저장 [ParkingAvailable - NotNull] [Controller] - Fail")
+	@WithMockCustomUser
+	void t18() throws Exception {
+		// Given
+		ShipRequest.Create givenRequestDto = createArbitraryBuilder
+			.set("parkingAvailable", null)
+			.sample();
+		// When
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/ship")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(givenRequestDto)));
+		// Then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.timestamp").exists())
+			.andExpect(jsonPath("$.code").value(GlobalErrorCode.NOT_VALID.getCode()))
+			.andExpect(jsonPath("$.data[0].field").value("parkingAvailable"))
+			.andExpect(jsonPath("$.data[0].reason").value("주차 여부는 필수 항목입니다."))
+			.andExpect(jsonPath("$.message").value("요청하신 유효성 검증에 실패하였습니다."))
+			.andExpect(jsonPath("$.success").value(false));
 	}
 }
