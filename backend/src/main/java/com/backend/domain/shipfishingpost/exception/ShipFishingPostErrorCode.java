7d112474e7f8b5f62fbc3e4ef07a99f5c638bf00
swjoon <jetkid1446@gmail.com>
2025-04-07 14:07:32 +0900

    feat: 선상낚시 에러 메세지 추가 (MIKKI-132)

--- backend/src/main/java/com/backend/domain/shipfishingpost/exception/ShipFishingPostErrorCode.java
+++ backend/src/main/java/com/backend/domain/shipfishingpost/exception/ShipFishingPostErrorCode.java
@@ -11,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ShipFishingPostErrorCode implements ErrorCode {
 
-	POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, 7001, "게시글 정보가 존재하지 않음");
+	POSTS_NOT_FOUND(HttpStatus.NOT_FOUND, 7001, "게시글 정보가 존재하지 않음"),
+	POSTS_RESERVATION_EXIST(HttpStatus.CONFLICT, 7002, "게시글의 남은 예약이 존재하므로 삭제할 수 없습니다."),
+	NOT_AUTHORITY_POSTS(HttpStatus.FORBIDDEN, 7003, "해당 게시글에 대한 권한이 없습니다.");;
 
 	private final HttpStatus httpStatus;
 	private final Integer code;
