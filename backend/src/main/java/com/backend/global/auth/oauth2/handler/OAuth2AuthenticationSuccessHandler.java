4d36d483ddb334c794997572ba0acbba56cd336e
Kim Dong O <kdo_1999@naver.com>
2025-04-07 12:09:28 +0900

    refactor: redirect 수정

--- backend/src/main/java/com/backend/global/auth/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/backend/global/auth/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -101,7 +101,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 		GenericResponse<Map<String, Object>> successResponse
 			= GenericResponse.of(true, loginResponse, "OAuth2 로그인 성공");
 
-		response.sendRedirect("https://mikki.kr/");
+		response.sendRedirect("https://api.mikki.kr/swagger-ui/index.html");
 
 		AuthResponseUtil.success(response, accessTokenCookie, HttpServletResponse.SC_OK, successResponse, objectMapper);
 	}
