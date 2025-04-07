13b7dab4ac37f09e84eb4b9aa8755db7d1387939
swjoon <jetkid1446@gmail.com>
2025-04-07 13:57:57 +0900

    feat: 예약 일자 삭제 메서드 (MIKKI-132) - Service

--- backend/src/main/java/com/backend/domain/reservationdate/service/ReservationDateServiceImpl.java
+++ backend/src/main/java/com/backend/domain/reservationdate/service/ReservationDateServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -54,6 +55,14 @@ public class ReservationDateServiceImpl implements ReservationDateService {
 			.findUnAvailableDatesByStartDateBetweenEndDate(shipFishingPostId, date.get(0), date.get(1));
 
 		return ReservationDateConverter.fromUnAvailableDateList(unAvailableDateList);
+	}
+
+	@Async
+	@Override
+	@Transactional
+	public void deleteReservationDateList(Long shipFishingPostId) {
+
+		reservationDateRepository.deleteByShipFishingPostId(shipFishingPostId);
 	}
 
 	/**
