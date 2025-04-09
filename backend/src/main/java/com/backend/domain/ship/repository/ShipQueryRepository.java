89ea8cb0e743b05e251e63089433e0e494fb852a
Kim Dong O <kdo_1999@naver.com>
2025-04-09 14:54:43 +0900

    feat: 회원 ID로 저장된 선박 전체 조회 기능 구현 (MiKKI-149)

--- backend/src/main/java/com/backend/domain/ship/repository/ShipQueryRepository.java
+++ backend/src/main/java/com/backend/domain/ship/repository/ShipQueryRepository.java
@@ -2,8 +2,12 @@ package com.backend.domain.ship.repository;
 
 import static com.backend.domain.ship.entity.QShip.*;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import com.backend.domain.ship.dto.response.QShipResponse_ShipAll;
+import com.backend.domain.ship.dto.response.ShipResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -20,5 +24,12 @@ public class ShipQueryRepository {
 			.from(ship)
 			.where(ship.memberId.eq(memberId))
 			.fetchOne();
+	}
+	public List<ShipResponse.ShipAll> findShipAll(Long memberId) {
+		return jpaQueryFactory
+			.select(new QShipResponse_ShipAll(ship.shipId, ship.shipName, ship.departurePort))
+			.from(ship)
+			.where(ship.memberId.eq(memberId))
+			.fetch();
 	}
 }
