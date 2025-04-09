b5267801a966852d63a4dfe36b9f6d6d4a98bc45
Kim Dong O <kdo_1999@naver.com>
2025-04-10 00:10:33 +0900

    feat: Comment 엔티티 추가 (MIKKI-159)

---
+++ backend/src/main/java/com/backend/domain/comment/entity/Comment.java
@@ -0,0 +1,44 @@
+package com.backend.domain.comment.entity;
+
+import com.backend.global.baseentity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Table(name = "comments")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SuperBuilder
+@ToString
+public class Comment extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long commentId;
+
+	private Long parentId;
+
+	@Column(nullable = false)
+	private Long memberId;
+
+	@Column(nullable = false)
+	private Long fishingTripPostId;
+
+	@Column(nullable = false, length = 100, columnDefinition = "TEXT")
+	private String content;
+
+	@Column(nullable = false)
+	@Builder.Default
+	private Integer childCount = 0;
+}
