0b99281ff5410d78777d79a7a38977f44da881fa
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-10 01:00:48 +0900

    feat: Redis 캐싱 처리 추가(MIKKI-120)

--- backend/src/main/java/com/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/backend/global/config/RedisConfig.java
@@ -1,11 +1,15 @@
 package com.backend.global.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,5 +32,20 @@ public class RedisConfig {
 		template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 
 		return template;
+	}
+
+	@Bean
+	public RedisCacheConfiguration cacheConfiguration() {
+		return RedisCacheConfiguration.defaultCacheConfig()
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+			).disableCachingNullValues();
+	}
+
+	@Bean
+	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+		return RedisCacheManager.builder(redisConnectionFactory)
+			.cacheDefaults(cacheConfiguration())
+			.build();
 	}
 }
