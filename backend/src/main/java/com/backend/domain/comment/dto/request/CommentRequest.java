2a30cc6995262b9a779abd59d15a676655fc5075
Kim Dong O <kdo_1999@naver.com>
2025-04-10 21:00:09 +0900

    feat: 댓글 수정시 사용할 요청 객체 추가 (MIKKI-174)

--- backend/src/main/java/com/backend/domain/comment/dto/request/CommentRequest.java
+++ backend/src/main/java/com/backend/domain/comment/dto/request/CommentRequest.java
@@ -7,13 +7,14 @@ import jakarta.validation.constraints.Size;
 public class CommentRequest {
 
 	/**
-     * {@code
+	 * {@code
 	 * {
-	 *   "content": "어떤 물고기 잡을건가요??",
-	 *   "parentId": 1
+	 * "content": "어떤 물고기 잡을건가요??",
+	 * "parentId": 1
 	 * }
-	 *}
-	 * @param content 댓글 내용
+	 * }
+	 *
+	 * @param content  댓글 내용
 	 * @param parentId 부모 ID (Null 허용)
 	 */
 	public record Create(
@@ -27,16 +28,34 @@ public class CommentRequest {
 	}
 
 	/**
-     * {@code
+	 * {@code
 	 * {
-	 *   "parentId": 1
+	 * "parentId": 1
 	 * }
-	 *}
+	 * }
+	 *
 	 * @param parentId 부모 ID (Null 허용)
 	 */
 	public record Search(
 		@Schema(description = "부모 댓글 ID", example = "1")
 		Long parentId
+	) {
+	}
+
+	/**
+	 * {@code
+	 * {
+	 * "content": "내용 수정"
+	 * }
+	 * }
+	 *
+	 * @param content {@link String}
+	 */
+	public record Update(
+		@Size(min = 1, max = 100, message = "내용은 1자 이상 100자 이하여야 합니다.")
+		@NotBlank(message = "내용은 필수 항목입니다.")
+		@Schema(description = "댓글 내용", example = "어떤 물고기 잡을건가요?")
+		String content
 	) {
 	}
 }
