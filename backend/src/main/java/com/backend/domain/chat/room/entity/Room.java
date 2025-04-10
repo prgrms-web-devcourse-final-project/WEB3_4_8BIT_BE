3c923b94df9d2b62b2bb2af59182d0f1ca8ca535
vdvhk12 <vdvhk12@gmail.com>
2025-04-10 15:01:02 +0900

    feat: 채팅방 엔티티 추가 (MIKKI-166)

---
+++ backend/src/main/java/com/backend/domain/chat/room/entity/Room.java
@@ -0,0 +1,48 @@
+package com.backend.domain.chat.room.entity;
+
+import com.backend.global.baseentity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Table(
+	name = "rooms",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			name = "uk_room_target_id_type",
+			columnNames = {"target_id", "target_type"}
+		)
+	}
+)
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Room extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long roomId;
+
+	@Column(nullable = false)
+	private Long targetId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TargetType targetType;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Status status;
+}
