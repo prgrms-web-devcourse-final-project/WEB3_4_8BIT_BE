d7ad33c5e0f43a60d906fbb2ad1e2086593f7a39
Kim Dong O <kdo_1999@naver.com>
2025-04-10 09:40:32 +0900

    refactor: 코드리뷰 반영 (MIKKI-159)

--- backend/src/main/java/com/backend/domain/comment/controller/CommentController.java
+++ backend/src/main/java/com/backend/domain/comment/controller/CommentController.java
@@ -15,19 +15,23 @@ import com.backend.domain.comment.service.CommentService;
 import com.backend.global.auth.oauth2.CustomOAuth2User;
 import com.backend.global.dto.response.GenericResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/fishing-trip-post")
+@Tag(name = "댓글 API")
 public class CommentController {
 
 	private final CommentService commentService;
 
+
+	@Operation(summary = "댓글 추가하기", description = "댓글 추가시 사용하는 API")
 	@PostMapping("/{fishingTripPostId}/comment")
-	@Parameter
 	public ResponseEntity<GenericResponse<Void>> createComment(
 		@Parameter(description = "댓글을 달 동출 게시글 ID", example = "1")
 		@PathVariable final Long fishingTripPostId,
