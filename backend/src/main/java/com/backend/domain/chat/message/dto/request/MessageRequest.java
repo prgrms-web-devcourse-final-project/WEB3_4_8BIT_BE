4a7d824d5335b65599559be2df5af4f5c5e063f1
vdvhk12 <vdvhk12@gmail.com>
2025-04-11 00:42:55 +0900

    refactor: 메세지 요청 데이터에서 회원 정보는 제거  (MIKKI-171)
    - senderId, nickname

--- backend/src/main/java/com/backend/domain/chat/message/dto/request/MessageRequest.java
+++ backend/src/main/java/com/backend/domain/chat/message/dto/request/MessageRequest.java
@@ -6,8 +6,6 @@ import com.backend.domain.chat.message.entity.MessageType;
 
 public record MessageRequest(
 	Long roomId,
-	Long senderId,
-	String senderNickname,
 	String content,
 	List<Long> fileIds,
 	MessageType type
