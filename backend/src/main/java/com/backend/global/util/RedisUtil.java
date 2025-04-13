b7f943af462b9644c56cbf3c1310306f8fd37f45
Hyeonsuk <vkdnjdjxor@naver.com>
2025-04-13 17:28:08 +0900

    refactor: @Casheable을 사용해서 불필요한 코드 제거(MIKKI-203)

--- backend/src/main/java/com/backend/global/util/RedisUtil.java
+++ backend/src/main/java/com/backend/global/util/RedisUtil.java
@@ -88,26 +88,6 @@ public class RedisUtil {
 	}
 
 	/**
-	 * 해당 키가 Redis 존재 여부 확인
-	 *
-	 * @param key Redis 키
-	 * @return 존재하면 true, 없으면 false
-	 */
-	public boolean hasKey(final String key) {
-		return Boolean.TRUE.equals(redisTemplate.hasKey(key));
-	}
-
-	/**
-	 * Redis 해당 키값 value 세팅
-	 *
-	 * @param key   Redis 키
-	 * @param value 키에 넣을 값
-	 */
-	public void setValue(final String key, final String value) {
-		redisTemplate.opsForValue().set(key, value);
-	}
-
-	/**
 	 * key Redis 존재시 삭제
 	 *
 	 * @param key 삭제할 Redis key
