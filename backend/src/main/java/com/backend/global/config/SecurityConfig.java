41eb0e915b6afa5be4f829c20b86f5626981012f
Kim Dong O <kdo_1999@naver.com>
2025-04-15 03:33:05 +0900

    refactor: 인가 권한 수정

--- backend/src/main/java/com/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/global/config/SecurityConfig.java
@@ -91,7 +91,8 @@ public class SecurityConfig {
 						"/api/v1/regions",
 						"/api/v1/fish-points",
 						"/api/v1/fish-points/**",
-						"/api/v1/chats"
+						"/api/v1/chats",
+						"/api/v1/reservation-dates/**"
 					).permitAll()
 
 					.requestMatchers("/api/v1/likes/**").hasAnyRole("USER", "CAPTAIN", "ADMIN")
