db8efadfcd5e7b99aa93aa9be603c68477dc0ca8
swjoon <jetkid1446@gmail.com>
2025-04-17 18:04:52 +0900

    test: 테스트 코드 수정 (MIKKI-215)

--- backend/src/test/java/com/backend/domain/reservation/service/ReservationConcurrencyTest.java
+++ backend/src/test/java/com/backend/domain/reservation/service/ReservationConcurrencyTest.java
@@ -11,7 +11,6 @@ import java.util.TimeZone;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -27,7 +26,6 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.backend.domain.reservation.dto.request.ReservationRequest;
-import com.backend.domain.reservation.dto.response.ReservationResponse;
 import com.backend.domain.reservationdate.entity.ReservationDate;
 import com.backend.domain.reservationdate.repository.ReservationDateRepository;
 import com.backend.domain.shipfishingpost.entity.ShipFishingPost;
@@ -207,141 +205,141 @@ public class ReservationConcurrencyTest extends BaseTest {
 		assertThat(exceptionOccurred).isTrue();
 	}
 
-	@Test
-	@DisplayName("동일 예약일에 8명이 동시 예약 요청 시 비관적 락을 통한 동시성 제어 테스트 [6명 실패] [Service] - Success")
-	void t02() throws Exception {
-		// Given
-		int threadCount = 8;
-
-		List<Runnable> tasks = new ArrayList<>();
-
-		for (int i = 0; i < threadCount; i++) {
-			tasks.add(() -> {
-				ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId,
-					givenDate, 6);
-
-				reservationService.createReservation(requestDto, ThreadLocalRandom.current().nextLong(1000));
-			});
-		}
-
-		// When
-		List<AtomicReference<Throwable>> exceptions = runConcurrentTasks(tasks);
-
-		// Then
-		long errorCount = exceptions.stream().filter(ref -> ref.get() != null).count();
-		assertThat(errorCount).isEqualTo(6);
-	}
-
-	@Test
-	@DisplayName("동일 예약일에 4명이 동시 예약 취소 시 비관적 락을 통한 동시성 제어 테스트 [Service] - Success")
-	void t03() throws Exception {
-		// Given
-		int threadCount = 4;
-
-		int guestCount = 2;
-
-		List<Long> reservationIdList = new ArrayList<>();
-		List<Long> memberIdList = new ArrayList<>();
-
-		for (long i = 1; i <= threadCount; i++) {
-			ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId, givenDate,
-				guestCount);
-
-			ReservationResponse.Detail detail = reservationService.createReservation(requestDto, i);
-
-			log.debug("예약 생성 : {} {}", detail.memberId(), detail.reservationId());
-
-			memberIdList.add(detail.memberId());
-			reservationIdList.add(detail.reservationId());
-		}
-
-		ReservationDate savedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
-				givenShipFishingPostId, givenDate)
-			.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
-
-		log.debug("차감 후 : {}", savedReservationDate.getRemainCount());
-
-		assertThat(savedReservationDate.getRemainCount()).isEqualTo(6);
-
-		List<Runnable> tasks = new ArrayList<>();
-
-		for (int i = 0; i < threadCount; i++) {
-			final int now = i;
-			tasks.add(() -> {
-				log.debug("예약 적용 : {} {}", reservationIdList.get(now), memberIdList.get(now));
-				reservationService.updateReservation(reservationIdList.get(now), memberIdList.get(now));
-			});
-		}
-
-		// When
-		List<AtomicReference<Throwable>> exceptions = runConcurrentTasks(tasks);
-
-		// Then
-		ReservationDate updatedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
-				givenShipFishingPostId, givenDate)
-			.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
-
-		long errorCount = exceptions.stream().filter(ref -> ref.get() != null).count();
-
-		log.debug("에러 개수 : {}", errorCount);
-
-		assertThat(updatedReservationDate.getRemainCount()).isEqualTo(14);
-	}
-
-	@Test
-	@DisplayName("동일 예약일에 4명이 동시 예약 취소 & 2명이 동시 예약 비관적 락을 통한 동시성 제어 테스트 [Service] - Success")
-	void t04() throws Exception {
-		// Given
-		int guestCount = 2;
-
-		int cancelCount = 4;
-		int createCount = 2;
-
-		List<Long> reservationIdList = new ArrayList<>();
-		List<Long> memberIdList = new ArrayList<>();
-
-		for (long i = 1; i <= cancelCount; i++) {
-			ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId, givenDate,
-				guestCount);
-
-			ReservationResponse.Detail detail = reservationService.createReservation(requestDto, i);
-
-			memberIdList.add(detail.memberId());
-			reservationIdList.add(detail.reservationId());
-		}
-
-		ReservationDate savedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
-				givenShipFishingPostId, givenDate)
-			.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
-
-		assertThat(savedReservationDate.getRemainCount()).isEqualTo(6);
-
-		List<Runnable> tasks = new ArrayList<>();
-
-		for (int i = 0; i < cancelCount; i++) {
-			final int now = i;
-			tasks.add(() -> {
-				reservationService.updateReservation(reservationIdList.get(now), memberIdList.get(now));
-			});
-		}
-
-		for (int i = 0; i < createCount; i++) {
-			tasks.add(() -> {
-				ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId, givenDate,
-					guestCount);
-
-				reservationService.createReservation(requestDto, ThreadLocalRandom.current().nextLong(1000));
-			});
-		}
-
-		List<AtomicReference<Throwable>> exceptions = runConcurrentTasks(tasks);
-
-		// Then
-		ReservationDate updatedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
-				givenShipFishingPostId, givenDate)
-			.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
-
-		assertThat(updatedReservationDate.getRemainCount()).isEqualTo(10);
-	}
+	// @Test
+	// @DisplayName("동일 예약일에 8명이 동시 예약 요청 시 비관적 락을 통한 동시성 제어 테스트 [6명 실패] [Service] - Success")
+	// void t02() throws Exception {
+	// 	// Given
+	// 	int threadCount = 8;
+	//
+	// 	List<Runnable> tasks = new ArrayList<>();
+	//
+	// 	for (int i = 0; i < threadCount; i++) {
+	// 		tasks.add(() -> {
+	// 			ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId,
+	// 				givenDate, 6);
+	//
+	// 			reservationService.createReservation(requestDto, ThreadLocalRandom.current().nextLong(1000));
+	// 		});
+	// 	}
+	//
+	// 	// When
+	// 	List<AtomicReference<Throwable>> exceptions = runConcurrentTasks(tasks);
+	//
+	// 	// Then
+	// 	long errorCount = exceptions.stream().filter(ref -> ref.get() != null).count();
+	// 	assertThat(errorCount).isEqualTo(6);
+	// }
+	//
+	// @Test
+	// @DisplayName("동일 예약일에 4명이 동시 예약 취소 시 비관적 락을 통한 동시성 제어 테스트 [Service] - Success")
+	// void t03() throws Exception {
+	// 	// Given
+	// 	int threadCount = 4;
+	//
+	// 	int guestCount = 2;
+	//
+	// 	List<Long> reservationIdList = new ArrayList<>();
+	// 	List<Long> memberIdList = new ArrayList<>();
+	//
+	// 	for (long i = 1; i <= threadCount; i++) {
+	// 		ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId, givenDate,
+	// 			guestCount);
+	//
+	// 		ReservationResponse.Detail detail = reservationService.createReservation(requestDto, i);
+	//
+	// 		log.debug("예약 생성 : {} {}", detail.memberId(), detail.reservationId());
+	//
+	// 		memberIdList.add(detail.memberId());
+	// 		reservationIdList.add(detail.reservationId());
+	// 	}
+	//
+	// 	ReservationDate savedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
+	// 			givenShipFishingPostId, givenDate)
+	// 		.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
+	//
+	// 	log.debug("차감 후 : {}", savedReservationDate.getRemainCount());
+	//
+	// 	assertThat(savedReservationDate.getRemainCount()).isEqualTo(6);
+	//
+	// 	List<Runnable> tasks = new ArrayList<>();
+	//
+	// 	for (int i = 0; i < threadCount; i++) {
+	// 		final int now = i;
+	// 		tasks.add(() -> {
+	// 			log.debug("예약 적용 : {} {}", reservationIdList.get(now), memberIdList.get(now));
+	// 			reservationService.updateReservation(reservationIdList.get(now), memberIdList.get(now));
+	// 		});
+	// 	}
+	//
+	// 	// When
+	// 	List<AtomicReference<Throwable>> exceptions = runConcurrentTasks(tasks);
+	//
+	// 	// Then
+	// 	ReservationDate updatedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
+	// 			givenShipFishingPostId, givenDate)
+	// 		.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
+	//
+	// 	long errorCount = exceptions.stream().filter(ref -> ref.get() != null).count();
+	//
+	// 	log.debug("에러 개수 : {}", errorCount);
+	//
+	// 	assertThat(updatedReservationDate.getRemainCount()).isEqualTo(14);
+	// }
+	//
+	// @Test
+	// @DisplayName("동일 예약일에 4명이 동시 예약 취소 & 2명이 동시 예약 비관적 락을 통한 동시성 제어 테스트 [Service] - Success")
+	// void t04() throws Exception {
+	// 	// Given
+	// 	int guestCount = 2;
+	//
+	// 	int cancelCount = 4;
+	// 	int createCount = 2;
+	//
+	// 	List<Long> reservationIdList = new ArrayList<>();
+	// 	List<Long> memberIdList = new ArrayList<>();
+	//
+	// 	for (long i = 1; i <= cancelCount; i++) {
+	// 		ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId, givenDate,
+	// 			guestCount);
+	//
+	// 		ReservationResponse.Detail detail = reservationService.createReservation(requestDto, i);
+	//
+	// 		memberIdList.add(detail.memberId());
+	// 		reservationIdList.add(detail.reservationId());
+	// 	}
+	//
+	// 	ReservationDate savedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
+	// 			givenShipFishingPostId, givenDate)
+	// 		.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
+	//
+	// 	assertThat(savedReservationDate.getRemainCount()).isEqualTo(6);
+	//
+	// 	List<Runnable> tasks = new ArrayList<>();
+	//
+	// 	for (int i = 0; i < cancelCount; i++) {
+	// 		final int now = i;
+	// 		tasks.add(() -> {
+	// 			reservationService.updateReservation(reservationIdList.get(now), memberIdList.get(now));
+	// 		});
+	// 	}
+	//
+	// 	for (int i = 0; i < createCount; i++) {
+	// 		tasks.add(() -> {
+	// 			ReservationRequest.Reserve requestDto = createReservationRequest(givenShipFishingPostId, givenDate,
+	// 				guestCount);
+	//
+	// 			reservationService.createReservation(requestDto, ThreadLocalRandom.current().nextLong(1000));
+	// 		});
+	// 	}
+	//
+	// 	List<AtomicReference<Throwable>> exceptions = runConcurrentTasks(tasks);
+	//
+	// 	// Then
+	// 	ReservationDate updatedReservationDate = reservationDateRepository.findByShipFishingPostIdAndReservationDate(
+	// 			givenShipFishingPostId, givenDate)
+	// 		.orElseThrow(() -> new RuntimeException("ReservationDate not found"));
+	//
+	// 	assertThat(updatedReservationDate.getRemainCount()).isEqualTo(10);
+	// }
 
 }
